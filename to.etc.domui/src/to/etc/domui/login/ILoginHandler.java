b4c2f4909ab9cc24931e297a250a8884ad2981a1
Frits Jalvingh <jal@etc.to>
2009-04-14 19:50:42 +0200

    Start login generalization

---
+++ to.etc.domui/src/to/etc/domui/login/ILoginHandler.java
@@ -0,0 +1,13 @@
+package to.etc.domui.login;
+
+/**
+ * Checks the user's access and if granted returns a IUser for the credentials
+ * passed. If not correct this returns null. The IUser returned will be cached
+ * in the session.
+ *
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on Apr 14, 2009
+ */
+public interface ILoginHandler {
+	IUser			loginUser(final String userid, final String credentials);
+}
---
+++ to.etc.domui/src/to/etc/domui/login/IUser.java
@@ -0,0 +1,30 @@
+package to.etc.domui.login;
+
+import java.util.*;
+
+/**
+ * Represents a logged-in user. This base interface only knows data that must be known about
+ * any logged-in user. Extras can be obtained if you know the implementation.
+ *
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on Apr 14, 2009
+ */
+public interface IUser {
+	/**
+	 * The user's login ID.
+	 * @return
+	 */
+	String			getLoginID();
+
+	/**
+	 * Return a display name for the user; this usually is the full formal name.
+	 * @return
+	 */
+	String			getDisplayName();
+
+	/**
+	 * The set of names representing the rights the user has.
+	 * @return
+	 */
+	Set<String>		getRightNames();
+}
