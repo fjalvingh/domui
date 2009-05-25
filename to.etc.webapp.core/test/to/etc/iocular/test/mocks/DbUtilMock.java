d3766e2204c9f99778952ebed371d1dc05fb3213
Frits Jalvingh <jal@etc.to>
2009-05-25 18:24:08 +0200

    Expose bug in parameter checking for factory destroyers

--- to.etc.webapp.core/test/to/etc/iocular/test/mocks/DbUtilMock.java
+++ to.etc.webapp.core/test/to/etc/iocular/test/mocks/DbUtilMock.java
@@ -1,11 +1,13 @@
 package to.etc.iocular.test.mocks;
 
+import to.etc.webapp.query.*;
+
 public class DbUtilMock {
-	static public DataContextMock			createContext() {
+	static public QDataContext			createContext() {
 		return new DataContextMock();
 	}
 
-	static public void						discardContext(final DataContextMock dcm) {
+	static public void discardContext(final DataContextMock dcm) {
 		dcm.decrement();
 	}
 
