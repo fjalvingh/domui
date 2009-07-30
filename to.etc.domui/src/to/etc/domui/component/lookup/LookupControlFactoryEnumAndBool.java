d43dc02b1c4a0dde51713e4ba046e3da830156cb
Frits Jalvingh <jal@etc.to>
2009-07-31 00:10:53 +0200

    Add missing files from execom merge

---
+++ to.etc.domui/src/to/etc/domui/component/lookup/LookupControlFactoryEnumAndBool.java
@@ -0,0 +1,56 @@
+package to.etc.domui.component.lookup;
+
+import java.util.*;
+
+import to.etc.domui.component.input.*;
+import to.etc.domui.component.meta.*;
+import to.etc.domui.component.meta.impl.*;
+import to.etc.webapp.nls.*;
+import to.etc.webapp.query.*;
+
+public class LookupControlFactoryEnumAndBool implements LookupControlFactory {
+
+	public int accepts(PropertyMetaModel pmm) {
+		Class< ? > iclz = pmm.getActualType();
+		return iclz == Boolean.class || iclz == Boolean.TYPE || Enum.class.isAssignableFrom(iclz) ? 2 : 0;
+	}
+
+	public LookupFieldQueryBuilderThingy createControl(SearchPropertyMetaModel spm, final PropertyMetaModel pmm) {
+
+		// Create a domainvalued combobox by default.
+		Object[] vals = pmm.getDomainValues();
+		ClassMetaModel ecmm = null;
+		List<ComboFixed.Pair<Object>> vl = new ArrayList<ComboFixed.Pair<Object>>();
+		for(Object o : vals) {
+			String label = pmm.getDomainValueLabel(NlsContext.getLocale(), o); // Label known to property?
+			if(label == null) {
+				if(ecmm == null)
+					ecmm = MetaManager.findClassMeta(pmm.getActualType()); // Try to get the property's type.
+				label = ecmm.getDomainLabel(NlsContext.getLocale(), o);
+				if(label == null)
+					label = o == null ? "" : o.toString();
+			}
+			vl.add(new ComboFixed.Pair<Object>(o, label));
+		}
+
+		final ComboFixed< ? > c = new ComboFixed<Object>(vl);
+		if(pmm.isRequired()) {
+			c.setMandatory(true);
+		}
+		String s = pmm.getDefaultHint();
+		if(s != null) {
+			c.setTitle(s);
+		}
+		return new DefaultLookupThingy(c) {
+			@Override
+			public boolean appendCriteria(QCriteria< ? > crit) throws Exception {
+				Object value = c.getValue();
+				if(value != null) {
+					crit.eq(pmm.getName(), value);
+					return true;
+				}
+				return true; // Okay but no data
+			}
+		};
+	}
+}
---
+++ to.etc.domui/src/to/etc/domui/converter/EnumConverter.java
@@ -0,0 +1,34 @@
+package to.etc.domui.converter;
+
+import java.util.*;
+
+import to.etc.domui.component.meta.*;
+import to.etc.domui.trouble.*;
+import to.etc.domui.util.*;
+import to.etc.webapp.nls.*;
+
+/**
+ * This generic converter for enums should be used only as convertObjectToString renderer.
+ * It is used for default rendering of enum fields inside table results.
+ * Backward conversion using  convertStringToObject is not supported and would throw exception.   
+ *
+ * @author vmijic
+ * Created on 29 Jul 2009
+ */
+public class EnumConverter<E extends Enum<E>> implements IConverter<E> {
+
+	public String convertObjectToString(Locale loc, E in) throws UIException {
+		if(in == null)
+			return "";
+		ClassMetaModel ecmm = MetaManager.findClassMeta(in.getClass());
+		if(ecmm != null) {
+			return ecmm.getDomainLabel(NlsContext.getLocale(), in);
+		}
+		return String.valueOf(in);
+	}
+
+	public E convertStringToObject(Locale loc, String input) throws UIException {
+		throw new ValidationException(Msgs.UNEXPECTED_EXCEPTION);
+	}
+
+}
