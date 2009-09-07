41b63b70c111f0343b227e7b3397f6fbdfc9b055
Vladimir Mijic <vmijic@execom.eu>
2009-09-07 19:20:04 +0200

    Fixed reading of case sensitive hint parameters. Added standard java logger to MetaUtils.

--- to.etc.domui/src/to/etc/domui/component/meta/MetaUtils.java
+++ to.etc.domui/src/to/etc/domui/component/meta/MetaUtils.java
@@ -1,5 +1,7 @@
 package to.etc.domui.component.meta;
 
+import java.util.logging.*;
+
 
 /**
  * Utility class used as utility method library for working with meta data.
@@ -7,6 +9,8 @@ package to.etc.domui.component.meta;
  * Created on 4 Aug 2009
  */
 public class MetaUtils {
+	private static final Logger logger = Logger.getLogger(MetaUtils.class.getName());
+
 	/**
 	 * Parses value of param defined in metadataLine string as Integer value.
 	 * @return Integer value represented as string, in case that param is not defined or NumberFormatException exception returns default value.
@@ -16,14 +20,14 @@ public class MetaUtils {
 		try {
 			return Integer.parseInt(paramValue);
 		} catch(NumberFormatException e) {
-			//FIXME: introduce log4j logging
+			logger.log(Level.ALL, e.getMessage());
 			return defaultValue;
 		}
 	}
 
 	public static String parseStringParam(final String metadataLine, final String paramName) {
 		String paramValue = null;
-		final String paramItem = paramName + '=';
+		final String paramItem = paramName.toLowerCase() + '=';
 		if(metadataLine.contains(paramItem)) {
 			if(metadataLine.indexOf(";", metadataLine.indexOf(paramItem)) > 0) {
 				paramValue = metadataLine.substring(metadataLine.indexOf(paramItem) + paramItem.length(), metadataLine.indexOf(";", metadataLine.indexOf(paramItem)));
