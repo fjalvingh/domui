d918a017f32af2d3a60d052a3371f1ddc7de7d40
Frits Jalvingh <jal@etc.to>
2009-10-19 18:59:41 +0200

    CSS standard version of progress box done, of course it does not work on IE as usual

--- to.etc.domui/src/to/etc/domui/component/misc/PercentageCompleteRuler.java
+++ to.etc.domui/src/to/etc/domui/component/misc/PercentageCompleteRuler.java
@@ -5,6 +5,18 @@ import to.etc.domui.dom.html.*;
 public class PercentageCompleteRuler extends Div {
 	private int m_percentage;
 
+	private int m_pixelwidth;
+
+	public PercentageCompleteRuler() {
+		setWidth("100px");
+		m_pixelwidth = 100;
+	}
+
+	public void setWidth(int pixels) {
+		m_pixelwidth = pixels;
+		setWidth(pixels + "px");
+	}
+
 	@Override
 	public void createContent() throws Exception {
 		setCssClass("ui-pct-rlr");
@@ -28,5 +40,9 @@ public class PercentageCompleteRuler extends Div {
 
 	private void updateValues() {
 		setText(Integer.valueOf(m_percentage) + "%");
+
+		//-- Set background position.
+		int pxl = (m_percentage * m_pixelwidth / 100);
+		setBackgroundPosition((-400 + pxl) + "px 0px");
 	}
 }
