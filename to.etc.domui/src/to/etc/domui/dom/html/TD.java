0ac8914a39ecf8feb4e5d2d749cfe4dd78fbe203
Frits Jalvingh <jal@etc.to>
2010-01-04 14:34:33 +0100

    Fixes and extra code from Dennis merged. Fix missing font css properties that were not rendered. Fix missing changed() calls in TD.

--- to.etc.domui/src/to/etc/domui/dom/html/TD.java
+++ to.etc.domui/src/to/etc/domui/dom/html/TD.java
@@ -1,5 +1,7 @@
 package to.etc.domui.dom.html;
 
+import to.etc.domui.util.*;
+
 public class TD extends NodeContainer {
 	private TableVAlign m_valign;
 
@@ -12,6 +14,8 @@ public class TD extends NodeContainer {
 	private String m_cellHeight;
 
 	private String m_cellWidth;
+
+	private TDAlignType m_align;
 
 	public TD() {
 		super("td");
@@ -31,7 +35,10 @@ public class TD extends NodeContainer {
 	}
 
 	public void setValign(TableVAlign valign) {
+		if(m_valign != valign)
+			return;
 		m_valign = valign;
+		changed();
 	}
 
 	public int getColspan() {
@@ -39,7 +46,10 @@ public class TD extends NodeContainer {
 	}
 
 	public void setColspan(int colspan) {
+		if(m_colspan == colspan)
+			return;
 		m_colspan = colspan;
+		changed();
 	}
 
 	public int getRowspan() {
@@ -47,7 +57,10 @@ public class TD extends NodeContainer {
 	}
 
 	public void setRowspan(int rowspan) {
+		if(m_rowspan == rowspan)
+			return;
 		m_rowspan = rowspan;
+		changed();
 	}
 
 	public boolean isNowrap() {
@@ -55,7 +68,10 @@ public class TD extends NodeContainer {
 	}
 
 	public void setNowrap(boolean nowrap) {
+		if(m_nowrap == nowrap)
+			return;
 		m_nowrap = nowrap;
+		changed();
 	}
 
 	public String getCellHeight() {
@@ -63,7 +79,10 @@ public class TD extends NodeContainer {
 	}
 
 	public void setCellHeight(String cellHeight) {
+		if(DomUtil.isEqual(m_cellHeight, cellHeight))
+			return;
 		m_cellHeight = cellHeight;
+		changed();
 	}
 
 	public String getCellWidth() {
@@ -71,6 +90,20 @@ public class TD extends NodeContainer {
 	}
 
 	public void setCellWidth(String cellWidth) {
+		if(DomUtil.isEqual(m_cellWidth, cellWidth))
+			return;
 		m_cellWidth = cellWidth;
+		changed();
+	}
+
+	public TDAlignType getAlign() {
+		return m_align;
+	}
+
+	public void setAlign(TDAlignType align) {
+		if(m_align == align)
+			return;
+		m_align = align;
+		changed();
 	}
 }
