6666a86f3926766416faed31e75ab8f018cfcc5a
Vladimir Mijic <vmijic@execom.eu>
2010-01-06 13:48:53 +0100

    Changed defaults for some flags.

--- to.etc.domui/src/to/etc/domui/dom/html/ModificationFenceDiv.java
+++ to.etc.domui/src/to/etc/domui/dom/html/ModificationFenceDiv.java
@@ -1,20 +1,27 @@
 package to.etc.domui.dom.html;
 
 /**
- * 
- * 
+ * This is DIV that is used as user input modified flag fence.
+ * Usually it is used to ignore input controls modification in some screen region.
+ * When components are used for read only data presentation purposes (like viewers of selection filters)
+ * those components does not modify any page value content.
  *
  * @author <a href="mailto:imilovanovic@execom.eu">Igor Milovanović</a>
  * Created on Dec 8, 2009
  */
 public class ModificationFenceDiv extends Div implements IUserInputModifiedFence {
 
-	private boolean m_finalUserInputModifiedFence;
+	private boolean m_finalUserInputModifiedFence = true;
 
 	private boolean m_modified;
 
-	private boolean m_ignoreModifiedInputs;
+	private boolean m_ignoreModifiedInputs = true;
 
+	/**
+	 * Indicates wether component keep tracks on its childs modifications.
+	 * By default set to true.
+	 * @return
+	 */
 	public boolean isIgnoreModifiedInputs() {
 		return m_ignoreModifiedInputs;
 	}
@@ -23,11 +30,18 @@ public class ModificationFenceDiv extends Div implements IUserInputModifiedFence
 		m_ignoreModifiedInputs = ignoreModifiedInputs;
 	}
 
+	/**
+	 * @see to.etc.domui.dom.html.IUserInputModifiedFence#isFinalUserInputModifiedFence()
+	 * By default set to true.
+	 */
 	@Override
 	public boolean isFinalUserInputModifiedFence() {
 		return m_finalUserInputModifiedFence;
 	}
 
+	/**
+	 * @see to.etc.domui.dom.html.IUserInputModifiedFence#isFinalUserInputModifiedFence()
+	 */
 	public void setFinalUserInputModifiedFence(boolean finalUserInputModifiedFence) {
 		m_finalUserInputModifiedFence = finalUserInputModifiedFence;
 	}
