7d81352fc957819eea5e3e0674450c6712e8d168
Frits Jalvingh <jal@etc.to>
2010-02-19 02:04:24 +0100

    Complete mini-editor.

--- to.etc.domui/src/to/etc/domui/component/fasthtml/HtmlMiniEditor.java
+++ to.etc.domui/src/to/etc/domui/component/fasthtml/HtmlMiniEditor.java
@@ -1,13 +1,31 @@
 package to.etc.domui.component.fasthtml;
 
+import to.etc.domui.dom.css.*;
 import to.etc.domui.dom.html.*;
 import to.etc.domui.state.*;
 import to.etc.domui.util.*;
 import to.etc.util.*;
 
+/**
+ * This is a small but very fast html editor. It shows way faster than
+ * the full Html editor but has a little less options. This uses a slightly
+ * adapter version of the <a href="http://code.google.com/p/jwysiwyg/">wysigyg</a>
+ * plugin.
+ *
+ * <p>One oddity in the code here is the handling of the "display" css property. The
+ * plugin adds a div just before the original textarea, then it makes the textarea
+ * display: none. The textarea is retained so the plugin can put it's content in there
+ * still. DomUI however will reset the display:none value after a value is entered
+ * because the changeAttribute call sent will clear it (the attribute is BLOCK in
+ * the DomUI DOM). To prevent this we set the attribute to BLOCK on a full render and
+ * reset it back no none as soon as a partial delta is to be rendered by listening
+ * for input on this control.
+ *
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on Feb 19, 2010
+ */
 public class HtmlMiniEditor extends TextArea {
 	private String m_styleSheet;
-
 
 	@Override
 	public void createContent() throws Exception {
@@ -22,6 +40,12 @@ public class HtmlMiniEditor extends TextArea {
 		//		appendCreateJS("$(\"#" + getActualID() + "\").wysiwyg({css:'/ui/$themes/blue/style.theme.css'});");
 	}
 
+	/**
+	 * Contains the in-editor stylesheet to use, which determines the presentation\
+	 * of the document inside the editor. If not set it defaults to
+	 * THEME/minieditor.css.
+	 * @return
+	 */
 	public String getStyleSheet() {
 		return PageContext.getRequestContext().translateResourceName(m_styleSheet);
 	}
@@ -31,5 +55,16 @@ public class HtmlMiniEditor extends TextArea {
 			return;
 		m_styleSheet = styleSheet;
 		changed();
+	}
+
+	@Override
+	public void onBeforeFullRender() throws Exception {
+		setDisplay(DisplayType.BLOCK);
+	}
+
+	@Override
+	public boolean acceptRequestParameter(String[] values) throws Exception {
+		setDisplay(DisplayType.NONE);
+		return super.acceptRequestParameter(values);
 	}
 }
