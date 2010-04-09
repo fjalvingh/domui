945f98c9009b114706c282416c27469c16548d24
Frits Jalvingh <jal@etc.to>
2010-04-09 23:27:56 +0200

    Experimental addition for data-conditional rights

--- to.etc.domui/src/to/etc/domui/login/IUser.java
+++ to.etc.domui/src/to/etc/domui/login/IUser.java
@@ -27,4 +27,17 @@ public interface IUser {
 	//	Set<String>		getRightNames();
 
 	boolean hasRight(String r);
+
+	/**
+	 * EXPERIMENTAL INTERFACE, DO NOT USE Determines if right r is enabled for the specified data element. The implementation
+	 * will decide how to map this. The dataElement can be a "primary element" meaning something that rights are explicitly
+	 * assigned on, or it can be something that can be linked to such a "priomary element". In the latter case it is the
+	 * implementation's responsibility to obtain the primary element from the data passed and apply the rights check there.
+	 * If data-bound permissions are not implemented this MUST return getRight(r).
+	 *
+	 * @param r
+	 * @param dataElement
+	 * @return
+	 */
+	boolean hasRight(String r, Object dataElement);
 }
