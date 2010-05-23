f0a9b77177d47f5ad2aa39816e37e6624ee6633f
Frits Jalvingh <jal@etc.to>
2010-05-23 19:02:45 +0200

    Documentation change

--- to.etc.domui/src/to/etc/domui/component/tbl/SimpleListModel.java
+++ to.etc.domui/src/to/etc/domui/component/tbl/SimpleListModel.java
@@ -3,6 +3,10 @@ package to.etc.domui.component.tbl;
 import java.util.*;
 
 /**
+ * DO NOT USE IF YOUR DATA CAN CHANGE AND YOU ARE NOT UPDATING THOSE CHANGES HERE!!
+ * This model uses a list to populate a table. It is meant to be used <i>only</i> if
+ * the resulting model is maintained by yourself.
+ *
  * Model for list-based data. The actual list instance will be maintained on updates.
  *
  * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
