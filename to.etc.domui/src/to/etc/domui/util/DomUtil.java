48e44724d30c9ae5b50c950725dd227053dc1b32
Frits Jalvingh <jal@etc.to>
2010-08-03 20:15:43 +0200

    Add method to open window using generic URL.

--- to.etc.domui/src/to/etc/domui/util/DomUtil.java
+++ to.etc.domui/src/to/etc/domui/util/DomUtil.java
@@ -1324,6 +1324,48 @@ final public class DomUtil {
 		return sb.toString();
 	}
 
+	static public String createOpenWindowJS(String url, WindowParameters newWindowParameters) {
+		//-- We need a NEW window session. Create it,
+		RequestContextImpl ctx = (RequestContextImpl) PageContext.getRequestContext();
+		WindowSession cm = ctx.getSession().createWindowSession();
+
+		//-- Send a special JAVASCRIPT open command, containing the shtuff.
+		StringBuilder sb = new StringBuilder();
+		sb.append("DomUI.openWindow('");
+		sb.append(url);
+		sb.append("','");
+		sb.append(cm.getWindowID());
+		sb.append("','");
+
+		sb.append("resizable=");
+		sb.append(newWindowParameters.isResizable() ? "yes" : "no");
+		sb.append(",scrollbars=");
+		sb.append(newWindowParameters.isShowScrollbars() ? "yes" : "no");
+		sb.append(",toolbar=");
+		sb.append(newWindowParameters.isShowToolbar() ? "yes" : "no");
+		sb.append(",location=");
+		sb.append(newWindowParameters.isShowLocation() ? "yes" : "no");
+		sb.append(",directories=");
+		sb.append(newWindowParameters.isShowDirectories() ? "yes" : "no");
+		sb.append(",status=");
+		sb.append(newWindowParameters.isShowStatus() ? "yes" : "no");
+		sb.append(",menubar=");
+		sb.append(newWindowParameters.isShowMenubar() ? "yes" : "no");
+		sb.append(",copyhistory=");
+		sb.append(newWindowParameters.isCopyhistory() ? "yes" : "no");
+
+		if(newWindowParameters.getWidth() > 0) {
+			sb.append(",width=");
+			sb.append(newWindowParameters.getWidth());
+		}
+		if(newWindowParameters.getHeight() > 0) {
+			sb.append(",height=");
+			sb.append(newWindowParameters.getHeight());
+		}
+		sb.append("');");
+		return sb.toString();
+	}
+
 	public static boolean isWhitespace(char c) {
 		return c == '\u00a0' || Character.isWhitespace(c);
 
