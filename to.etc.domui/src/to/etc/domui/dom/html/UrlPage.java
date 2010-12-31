5218b5e25f6287f682e0794396f743e2f935d359
Frits Jalvingh <jal@etc.to>
2010-12-31 15:54:36 +0100

    Remove now obsolete page bundle code

--- to.etc.domui/src/to/etc/domui/dom/html/UrlPage.java
+++ to.etc.domui/src/to/etc/domui/dom/html/UrlPage.java
@@ -24,9 +24,6 @@
  */
 package to.etc.domui.dom.html;
 
-import to.etc.domui.util.*;
-import to.etc.webapp.*;
-import to.etc.webapp.nls.*;
 
 /**
  * The base for all pages that can be accessed thru URL's. This is mostly a
@@ -38,9 +35,6 @@ import to.etc.webapp.nls.*;
  * Created on Sep 1, 2008
  */
 public class UrlPage extends Div {
-	/** Cached bundle for the page. If the bundle is not found this contains null.. */
-	private BundleRef m_pageBundle;
-
 	/** The title for the page in the head's TITLE tag. */
 	private String m_pageTitle;
 
@@ -54,34 +48,6 @@ public class UrlPage extends Div {
 	 * @throws Exception
 	 */
 	public void onDestroy() throws Exception {}
-
-	/**
-	 * Returns the bundle defined for the page. This defaults to a bundle with the
-	 * same name as the page's class name, but can be overridden by an @UIMenu
-	 * annotation on the root class.
-	 * @return
-	 */
-	public BundleRef getPageBundle() {
-		if(m_pageBundle == null) {
-			m_pageBundle = DomUtil.findPageBundle(this);
-			if(m_pageBundle == null)
-				throw new ProgrammerErrorException("The page " + this.getClass() + " does not have a page resource bundle");
-		}
-		return m_pageBundle;
-	}
-
-	/**
-	 * Lookup and format a message from the page's bundle.
-	 * @param key
-	 * @param param
-	 * @return
-	 */
-	public String $(String key, Object... param) {
-		BundleRef br = getPageBundle();
-		if(key.startsWith("~")) // Prevent silly bugs.
-			key = key.substring(1);
-		return br.formatMessage(key, param);
-	}
 
 	/**
 	 * Get the head's "title" tag for this page. This will show that title in the browser's top window.
