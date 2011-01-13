d62df91df3c31f035328022c98038899199995f5
Frits Jalvingh <jal@etc.to>
2011-01-13 22:05:23 +0100

    Add Javadoc for File upload code; add dateTime constructor to DateInput

--- to.etc.domui/src/to/etc/domui/component/upload/FileUpload.java
+++ to.etc.domui/src/to/etc/domui/component/upload/FileUpload.java
@@ -30,6 +30,7 @@ import to.etc.domui.component.buttons.*;
 import to.etc.domui.dom.html.*;
 import to.etc.domui.parts.*;
 import to.etc.domui.state.*;
+import to.etc.domui.util.*;
 import to.etc.domui.util.upload.*;
 
 /**
@@ -72,6 +73,11 @@ public class FileUpload extends Div {
 
 	public FileUpload() {}
 
+	/**
+	 * Create an upload item that acceps a max #of files and a set of extensions.
+	 * @param maxfiles
+	 * @param allowedExt
+	 */
 	public FileUpload(int maxfiles, String allowedExt) {
 		m_maxFiles = maxfiles;
 		m_allowedExtensions = allowedExt;
@@ -125,14 +131,32 @@ public class FileUpload extends Div {
 		}
 	}
 
+	/**
+	 * Internal: get the input type="file" thingy.
+	 * @return
+	 */
 	FileInput getInput() {
 		return m_input;
 	}
 
+	/**
+	 * Return the current value: the list of files that have been uploaded and
+	 * their related data. The {@link UploadItem} contains a reference to the
+	 * actual file {@link UploadItem#getFile()}; this file remains present only
+	 * while the page is still alive. If the page is destroyed all of it's uploaded
+	 * files will be deleted. So if you need to retain the file somehow after upload
+	 * it's contents needs to be <b>copied</b> to either another file that you control
+	 * or to a BLOB in a database.
+	 * @return
+	 */
 	public List<UploadItem> getFiles() {
 		return m_files;
 	}
 
+	/**
+	 * Return T if the max. #of files has been reached.
+	 * @return
+	 */
 	public boolean isFull() {
 		return m_files.size() >= m_maxFiles;
 	}
@@ -147,12 +171,24 @@ public class FileUpload extends Div {
 			forceRebuild();
 	}
 
+	/**
+	 * Return the space separated list of allowed file extensions.
+	 * @return
+	 */
 	public String getAllowedExtensions() {
 		return m_allowedExtensions;
 	}
 
+	/**
+	 * Set the list of allowed file extensions.
+	 * @param allowedExtensions
+	 */
 	public void setAllowedExtensions(String allowedExtensions) {
+		if(DomUtil.isEqual(allowedExtensions, m_allowedExtensions))
+			return;
+
 		m_allowedExtensions = allowedExtensions;
+		changed();
 	}
 
 	//	public int getMaxSize() {
@@ -163,10 +199,17 @@ public class FileUpload extends Div {
 	//		m_maxSize = maxSize;
 	//	}
 
+	/**
+	 * FIXME T if at least 1 file needs to be uploaded.
+	 */
 	public boolean isRequired() {
 		return m_required;
 	}
 
+	/**
+	 * Set to T if at least one file needs to have been uploaded.
+	 * @param required
+	 */
 	public void setRequired(boolean required) {
 		m_required = required;
 	}
--- to.etc.domui/src/to/etc/domui/themes/CurrentThemePart.java
+++ to.etc.domui/src/to/etc/domui/themes/CurrentThemePart.java
@@ -1,5 +1,10 @@
 package to.etc.domui.themes;
 
+import java.io.*;
+import java.util.*;
+
+import javax.annotation.*;
+
 import to.etc.domui.server.*;
 import to.etc.domui.server.parts.*;
 import to.etc.domui.util.resources.*;
@@ -12,23 +17,104 @@ import to.etc.domui.util.resources.*;
  * Created on Jan 12, 2011
  */
 public class CurrentThemePart implements IBufferedPartFactory, IUrlPart {
+	static public final String CURRENTTHEME = "currentTheme/";
+
+	static public final String SHEETNAME = "style.css";
+
+	static private class Key {
+		private String m_browserID;
+
+		private BrowserVersion m_bv;
+
+		private String m_name;
+
+		public Key(@Nonnull String name, @Nullable BrowserVersion bv) {
+			m_name = name;
+			m_bv = bv;
+			if(null != bv)
+				m_browserID = bv.getBrowserName() + "/" + bv.getMajorVersion();
+		}
+
+		@Override
+		public String toString() {
+			return "[stylesheet: browser=" + m_bv + "]";
+		}
+
+		public BrowserVersion getBrowserVersion() {
+			return m_bv;
+		}
+		public String getName() {
+			return m_name;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((m_browserID == null) ? 0 : m_browserID.hashCode());
+			result = prime * result + ((m_name == null) ? 0 : m_name.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if(this == obj)
+				return true;
+			if(obj == null)
+				return false;
+			if(getClass() != obj.getClass())
+				return false;
+			Key other = (Key) obj;
+			if(m_browserID == null) {
+				if(other.m_browserID != null)
+					return false;
+			} else if(!m_browserID.equals(other.m_browserID))
+				return false;
+			if(m_name == null) {
+				if(other.m_name != null)
+					return false;
+			} else if(!m_name.equals(other.m_name))
+				return false;
+			return true;
+		}
+	}
+
 	/**
 	 * Accept all RURLs that start with "currentTheme/".
 	 * @see to.etc.domui.server.parts.IUrlPart#accepts(java.lang.String)
 	 */
 	@Override
 	public boolean accepts(String rurl) {
-		return rurl.startsWith("currentTheme/");
+		return rurl.startsWith(CURRENTTHEME);
 	}
 
 	@Override
 	public Object decodeKey(String rurl, IExtendedParameterInfo param) throws Exception {
-		return rurl;
+		String name = rurl.substring(CURRENTTHEME.length());
+		BrowserVersion	bv = null;
+		if(name.equals(SHEETNAME)) {
+			bv = param.getBrowserVersion();
+		}
+		return new Key(name, bv);
 	}
 
 	@Override
 	public void generate(PartResponse pr, DomApplication da, Object key, ResourceDependencyList rdl) throws Exception {
+		Key k = (Key) key;
+		if(k.getName().equals(SHEETNAME)) {
+			//-- Export
+			if(!da.inDevelopmentMode()) { // Not gotten from WebContent or not in DEBUG mode? Then we may cache!
+				pr.setCacheTime(da.getDefaultExpiryTime());
+			}
 
+			DefaultThemeStore dts = (DefaultThemeStore) da.getTheme(rdl); // Get theme store and add it's dependencies to rdl
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(pr.getOutputStream()));
+			Map<String, Object>	map = new HashMap<String, Object>(dts.getThemeProperties());
+			map.put("browser", k.getBrowserVersion());
+			dts.getStylesheetTemplate().execute(pw, map);
+			pw.close();
+			pr.setMime("text/css");
+			return;
+		}
 	}
-
 }
