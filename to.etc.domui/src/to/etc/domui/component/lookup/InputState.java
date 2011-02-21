494c862afb6d3222f5c5132b0a07e5ba17253e0e
Igor Milovanovic <imilovanovic@execom.eu>
2011-02-21 16:55:24 +0100

    Added enum.

---
+++ to.etc.domui/src/to/etc/domui/component/lookup/InputState.java
@@ -0,0 +1,18 @@
+package to.etc.domui.component.lookup;
+
+
+/**
+ * Defines editable state of input components.
+ *
+ * @author <a href="mailto:imilovanovic@execom.eu">Igor Milovanović</a>
+ * Created on Feb 21, 2011
+ */
+enum InputState {
+
+	DEFAULT,
+
+	FORCE_EDITABLE,
+
+	FORCE_DISABLED;
+
+}
