dfc904f8b203d75baf5b36af22abee56ff2dab60
Dennis Bekkering <dennis.bekkering@itris.nl>
2011-08-02 02:17:06 +0200

    for sending messages to controls in business validations (changed)

---
+++ to.etc.domui/src/to/etc/domui/component/form/BindingMessenger.java
@@ -0,0 +1,53 @@
+package to.etc.domui.component.form;
+
+import to.etc.domui.dom.errors.*;
+import to.etc.domui.dom.html.*;
+import to.etc.webapp.nls.*;
+
+public final class BindingMessenger {
+
+	ModelBindings m_bindings;
+
+	BundleRef m_bundleRef;
+
+	public BindingMessenger(ModelBindings bindings, BundleRef bundleRef) {
+		super();
+		m_bindings = bindings;
+		m_bundleRef = bundleRef;
+	}
+
+	/**
+	 * Sends a message to the control that belongs to the object and property
+	 * @param object
+	 * @param property
+	 * @param message
+	 * @throws Exception 
+	 */
+	public void error(Object object, String property, String message, Object... param) throws Exception {
+		IControl[] h = new IControl[1];
+		find(h, object, property);
+		if(h[0] == null) {
+			throw new Exception(object.getClass().getSimpleName() + "." + property + " not found in bindings");
+		}
+		h[0].setMessage(UIMessage.error(m_bundleRef, message, param));
+	}
+
+	private void find(IControl[] h, Object object, String property) throws Exception {
+		for(IModelBinding mb : m_bindings) {
+			if(mb instanceof SimpleComponentPropertyBinding) {
+				SimpleComponentPropertyBinding b = (SimpleComponentPropertyBinding) mb;
+				System.err.println(b.getModel());
+				if(b.getModel().getValue().equals(object) && b.getPropertyMeta().getName().equals(property)) {
+					h[0] = b.getControl();
+					return;
+				}
+			} else if(mb instanceof ModelBindings) {
+				ModelBindings modelBindings = (ModelBindings) mb;
+				find(h, object, property);
+			}
+		}
+
+	}
+
+
+}
