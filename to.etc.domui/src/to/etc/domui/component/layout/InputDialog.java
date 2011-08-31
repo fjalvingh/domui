4b54856b4dc74a6435ed851e34c06ae96927c4ff
Vladimir Mijic <vmijic@execom.eu>
2011-08-31 14:28:07 +0200

    Added some more responsibility to InputDialog - it can be used now as is, out of box, since it has default layout.

--- to.etc.domui/src/to/etc/domui/component/layout/InputDialog.java
+++ to.etc.domui/src/to/etc/domui/component/layout/InputDialog.java
@@ -26,42 +26,50 @@ package to.etc.domui.component.layout;
 
 import javax.annotation.*;
 
+import to.etc.domui.dom.css.*;
 import to.etc.domui.dom.html.*;
 
 /**
- * A Dialog that is used to inout some data of type &lt;T&gt;, using input control of type &lt;C&gt;.
+ * A Dialog that is used to input single value of type &lt;T&gt;, using input control of type &lt;C&gt;.
+ * It uses default layout that shows [Save], [Cancel] buttons, and label and input control as main panel data.
+ * To customize layout, override {@link InputDialog#createFrame}.
  *
  *
  * @author <a href="mailto:vmijic@execom.eu">Vladimir Mijic</a>
  * Created on 30 Aug 2011
  */
 public class InputDialog<T, C extends NodeBase & IControl<T>> extends Dialog {
+
 	private C m_inputControl;
 
 	private T m_instance;
+
+	private String m_label;
 
 	public InputDialog(C inputControl, int width, int height, String title) {
 		super(width, height, title);
 		m_inputControl = inputControl;
 	}
 
-	public InputDialog(C inputControl, boolean modal, boolean resizable, int width, int height, String title) {
+	public InputDialog(C inputControl, boolean modal, boolean resizable, int width, int height, String title, String label) {
 		super(modal, resizable, width, height, title);
 		m_inputControl = inputControl;
+		m_label = label;
 	}
 
-	public InputDialog(C inputControl, boolean modal, boolean resizable, String title) {
-		super(modal, resizable, title);
+	public InputDialog(C inputControl, boolean modal, boolean resizable, String title, String label) {
+		super(modal, resizable, DEFWIDTH, MINHEIGHT, title);
 		m_inputControl = inputControl;
+		m_label = label;
 	}
 
 	public InputDialog(C inputControl, boolean resizable, String title) {
-		super(resizable, title);
+		super(true, resizable, DEFWIDTH, MINHEIGHT, title);
 		m_inputControl = inputControl;
 	}
 
 	public InputDialog(C inputControl, String title) {
-		super(title);
+		super(true, false, DEFWIDTH, MINHEIGHT, title);
 		m_inputControl = inputControl;
 	}
 
@@ -71,6 +79,8 @@ public class InputDialog<T, C extends NodeBase & IControl<T>> extends Dialog {
 	/**
 	 * Overridden to create the initial button bar with the "save" button and the
 	 * "cancel" buttons in place.
+	 * Also gives default input dialog layout.
+	 * Overide to set custom layout, but then adding buttons is your responsibility!
 	 *
 	 * @see to.etc.domui.component.layout.Window#createFrame()
 	 */
@@ -79,6 +89,15 @@ public class InputDialog<T, C extends NodeBase & IControl<T>> extends Dialog {
 	protected void createFrame() throws Exception {
 		super.createFrame();
 		createButtons();
+		Div pnl = new Div();
+		pnl.setMarginTop("25px");
+		if(m_label != null) {
+			pnl.add(new Label(m_label));
+			getInputControl().setMarginLeft("10px");
+		}
+		pnl.add(getInputControl());
+		add(pnl);
+		pnl.setTextAlign(TextAlign.CENTER);
 	}
 
 	/**
@@ -109,14 +128,40 @@ public class InputDialog<T, C extends NodeBase & IControl<T>> extends Dialog {
 	}
 
 	/**
-	 * Can be overridden to add extra validation logic - instance is provided using getInstance method.
-	 *
-	 * @see to.etc.domui.component.layout.Dialog#onSaveBind()
+	 * Finalized implementation, call to onSaveData with passed input instance.
+	 * @see to.etc.domui.component.layout.InputDialog#onSaveData(T)
+	 */
+	@Override
+	protected final boolean onSave() throws Exception {
+		return onSaveData(getInstance());
+	}
+
+	/**
+	 * Override to do save logic on instance before the close event is sent and the window is closed.
+	 * @param instance
+	 * @return
+	 */
+	protected boolean onSaveData(T instance) throws Exception {
+		return true;
+	}
+
+	/**
+	 * Finalized implementation, call to onValidateData with passed input instance.
+	 * @see to.etc.domui.component.layout.InputDialog#onValidateData(T)
 	 */
 	@OverridingMethodsMustInvokeSuper
 	@Override
-	protected boolean onValidate() throws Exception {
-		getInstance();
+	protected final boolean onValidate() throws Exception {
+		return onValidateData(getInstance());
+	}
+
+	/**
+	 * Override to do validation logic on instance before we continue to save method.
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	protected boolean onValidateData(T data) throws Exception {
 		return true;
 	}
 
@@ -130,5 +175,13 @@ public class InputDialog<T, C extends NodeBase & IControl<T>> extends Dialog {
 
 	public C getInputControl() {
 		return m_inputControl;
+	}
+
+	public String getLabel() {
+		return m_label;
+	}
+
+	public void setLabel(String label) {
+		m_label = label;
 	}
 }
