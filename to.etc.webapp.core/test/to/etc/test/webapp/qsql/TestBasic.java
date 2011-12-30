e6f57a89b1d94360493ea735ec0c94c0fbba847e
Frits Jalvingh <jal@etc.to>
2011-12-30 19:59:10 +0100

    Resurrect JUnit tests.

--- to.etc.webapp.core/test/to/etc/test/webapp/qsql/TestBasic.java
+++ to.etc.webapp.core/test/to/etc/test/webapp/qsql/TestBasic.java
@@ -24,101 +24,111 @@
  */
 package to.etc.test.webapp.qsql;
 
+import java.sql.*;
+import java.util.*;
+
+import javax.sql.*;
+
+import org.junit.*;
+
+import to.etc.webapp.qsql.*;
+import to.etc.webapp.query.*;
+import to.etc.webapp.testsupport.*;
+
 
 public class TestBasic {
-	//	static private DataSource m_ds;
+	static private DataSource m_ds;
 
-	//	static private QDataContext m_dc;
-	//
-	//	@BeforeClass
-	//	static public void setUp() throws Exception {
-	//		ConnectionPool pool = PoolManager.getInstance().definePool("vpdemo");
-	//		//		m_ds = pool.getUnpooledDataSource();
-	////		m_dc = TUtilSetupDB.getQDataContext();
-	//		Assert.fail("cycle dependancy with moca.database");
-	//	}
-	//
-	//	static <T> List<T> exec(JdbcQuery<T> q) throws Exception {
-	//		//		Connection dbc = m_ds.getConnection();
-	//		//		JdbcDataContext	jdc = new JdbcDataContext(null, dbc);
-	//		q.dump();
-	//		return (List<T>) q.query(m_dc);
-	//	}
-	//
-	//	static <T> List<T> exec(QCriteria<T> q) throws Exception {
-	//		JdbcQuery<T> jq = JdbcQuery.create(q);
-	//		return exec(jq);
-	//	}
-	//
-	//	@Test
-	//	public void testSQLGen1() throws Exception {
-	//		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class);
-	//		JdbcSQLGenerator gc = new JdbcSQLGenerator();
-	//		gc.visitCriteria(qc);
-	//
-	//		System.out.println(gc.getSQL());
-	//		Assert.assertEquals("select this_.ID,this_.grbr_code,this_.omschrijving,this_.grbr_type_omschrijving from v_dec_grootboekrekeningen this_", gc.getSQL());
-	//		Assert.assertEquals(1, gc.getRetrieverList().size());
-	//		Assert.assertEquals(0, gc.getValList().size());
-	//	}
-	//
-	//	@Test
-	//	public void testSQLGen2() throws Exception {
-	//		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class).eq("id", Long.valueOf(12)).eq("code", "BR12");
-	//
-	//		JdbcSQLGenerator gc = new JdbcSQLGenerator();
-	//		gc.visitCriteria(qc);
-	//
-	//		System.out.println(gc.getSQL());
-	//		Assert.assertEquals("select this_.ID,this_.grbr_code,this_.omschrijving,this_.grbr_type_omschrijving from v_dec_grootboekrekeningen this_ where this_.ID=? and this_.grbr_code=?", gc.getSQL());
-	//		Assert.assertEquals(1, gc.getRetrieverList().size());
-	//		Assert.assertEquals(2, gc.getValList().size());
-	//	}
-	//
-	//	@Test
-	//	public void testSQLGen3() throws Exception {
-	//		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class).eq("id", Long.valueOf(12)).eq("code", "BR12");
-	//		qc.ascending("code");
-	//		qc.descending("description");
-	//
-	//		JdbcSQLGenerator gc = new JdbcSQLGenerator();
-	//		gc.visitCriteria(qc);
-	//
-	//		System.out.println(gc.getSQL());
-	//		Assert
-	//			.assertEquals(
-	//				"select this_.ID,this_.grbr_code,this_.omschrijving,this_.grbr_type_omschrijving from v_dec_grootboekrekeningen this_ where this_.ID=? and this_.grbr_code=? order by this_.grbr_code asc,this_.omschrijving desc",
-	//				gc.getSQL());
-	//		Assert.assertEquals(1, gc.getRetrieverList().size());
-	//		Assert.assertEquals(2, gc.getValList().size());
-	//	}
-	//
-	//	@Test
-	//	public void testExec1() throws Exception {
-	//		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class);
-	//		List<LedgerAccount> res = exec(qc);
-	//
-	//		System.out.println("Got " + res.size() + " results");
-	//		int ix = 0;
-	//		for(LedgerAccount la : res) {
-	//			if(ix++ > 10)
-	//				break;
-	//			System.out.println("la: " + la.gweetCode() + ", " + la.getDescription() + ", " + la.getTypeDescription() + ", " + la.getId());
-	//		}
-	//		Assert.assertTrue(res.size() != 0);
-	//	}
-	//
-	//	@Test
-	//	public void testExec2() throws Exception {
-	//		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class).like("code", "E%");
-	//		List<LedgerAccount> res = exec(qc);
-	//
-	//		System.out.println("Got " + res.size() + " results");
-	//		//		int ix = 0;
-	//		for(LedgerAccount la : res) {
-	//			if(!la.getCode().startsWith("E"))
-	//				Assert.fail("Got code not starting with E: " + la.getCode());
-	//		}
-	//		Assert.assertTrue(res.size() != 0);
-	//	}
+	static private QDataContext m_dc;
+
+	@BeforeClass
+	static public void setUp() throws Exception {
+		m_ds = TUtilTestProperties.getRawDataSource();
+		Connection dbc = m_ds.getConnection();
+		m_dc = new JdbcDataContext(null, dbc);
+	}
+
+	static <T> List<T> exec(JdbcQuery<T> q) throws Exception {
+		//		Connection dbc = m_ds.getConnection();
+		//		JdbcDataContext	jdc = new JdbcDataContext(null, dbc);
+		q.dump();
+		return (List<T>) q.query(m_dc);
+	}
+
+	static <T> List<T> exec(QCriteria<T> q) throws Exception {
+		JdbcQuery<T> jq = JdbcQuery.create(q);
+		return exec(jq);
+	}
+
+	@Test
+	public void testSQLGen1() throws Exception {
+		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class);
+		JdbcSQLGenerator gc = new JdbcSQLGenerator();
+		gc.visitCriteria(qc);
+
+		System.out.println(gc.getSQL());
+		Assert.assertEquals("select this_.ID,this_.grbr_code,this_.omschrijving,this_.grbr_type_omschrijving from v_dec_grootboekrekeningen this_", gc.getSQL());
+		Assert.assertEquals(1, gc.getRetrieverList().size());
+		Assert.assertEquals(0, gc.getValList().size());
+	}
+
+	@Test
+	public void testSQLGen2() throws Exception {
+		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class).eq("id", Long.valueOf(12)).eq("code", "BR12");
+
+		JdbcSQLGenerator gc = new JdbcSQLGenerator();
+		gc.visitCriteria(qc);
+
+		System.out.println(gc.getSQL());
+		Assert.assertEquals("select this_.ID,this_.grbr_code,this_.omschrijving,this_.grbr_type_omschrijving from v_dec_grootboekrekeningen this_ where this_.ID=? and this_.grbr_code=?", gc.getSQL());
+		Assert.assertEquals(1, gc.getRetrieverList().size());
+		Assert.assertEquals(2, gc.getValList().size());
+	}
+
+	@Test
+	public void testSQLGen3() throws Exception {
+		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class).eq("id", Long.valueOf(12)).eq("code", "BR12");
+		qc.ascending("code");
+		qc.descending("description");
+
+		JdbcSQLGenerator gc = new JdbcSQLGenerator();
+		gc.visitCriteria(qc);
+
+		System.out.println(gc.getSQL());
+		Assert
+			.assertEquals(
+				"select this_.ID,this_.grbr_code,this_.omschrijving,this_.grbr_type_omschrijving from v_dec_grootboekrekeningen this_ where this_.ID=? and this_.grbr_code=? order by this_.grbr_code asc,this_.omschrijving desc",
+				gc.getSQL());
+		Assert.assertEquals(1, gc.getRetrieverList().size());
+		Assert.assertEquals(2, gc.getValList().size());
+	}
+
+	@Test
+	public void testExec1() throws Exception {
+		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class);
+		List<LedgerAccount> res = exec(qc);
+
+		System.out.println("Got " + res.size() + " results");
+		int ix = 0;
+		for(LedgerAccount la : res) {
+			if(ix++ > 10)
+				break;
+			System.out.println("la: " + la.getCode() + ", " + la.getDescription() + ", " + la.getTypeDescription() + ", " + la.getId());
+		}
+		Assert.assertTrue(res.size() != 0);
+	}
+
+	@Test
+	public void testExec2() throws Exception {
+		QCriteria<LedgerAccount> qc = QCriteria.create(LedgerAccount.class).like("code", "E%");
+		List<LedgerAccount> res = exec(qc);
+
+		System.out.println("Got " + res.size() + " results");
+		//		int ix = 0;
+		for(LedgerAccount la : res) {
+			if(!la.getCode().startsWith("E"))
+				Assert.fail("Got code not starting with E: " + la.getCode());
+		}
+		Assert.assertTrue(res.size() != 0);
+	}
 }
