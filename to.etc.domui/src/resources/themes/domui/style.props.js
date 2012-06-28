587eafa74870b791f9971be71db117c169f262cd
Vladimir Mijic <vmijic@execom.eu>
2012-06-28 17:44:59 +0200

    Added small adjustments to default parameters of domui theme

--- to.etc.domui/src/resources/themes/domui/style.props.js
+++ to.etc.domui/src/resources/themes/domui/style.props.js
@@ -1,5 +1,5 @@
 /*
- * "blue" stylesheet properties.
+ * "domui" stylesheet properties.
  */
 font_family='Verdana, Geneva, "DejaVu Sans", sans-serif';
 font_size="11px";
@@ -17,5 +17,28 @@ dpr_dis_first="THEME/dpr-first-dis.png.svg?w=20&h=20";
 dpr_dis_last="THEME/dpr-last-dis.png.svg?w=20&h=20";
 
 defaultbutton_height=23;
+window_title_font_size='15px';
 
+//-- input bevel: darker grey / lighter grey
+bevel_up="#ABADB3"
+bevel_down="#BFCDD9"
+
+//-- hovel input bevel for a component: dark blue/light blue
+bevel_hover_up="#5794BF"
+bevel_hover_down="#D7E8F8"
+
+//-- button color
+button_bg="#ff9436"
+button_text_color="#ffffff"
+
+//-- Header colors
+header_bg="#ff9436"
+header_bg2="#ffffff"
+header_text_color="#ffffff"
+
+//-- Error colors (error panel)
+error_bg="#f7c2c2"
+
+//-- selected items background
+selected_bg="#ff9436"
 
