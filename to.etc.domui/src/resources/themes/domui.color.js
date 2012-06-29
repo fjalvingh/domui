b73953928bcc19f85be1d95d09a2d17895b28358
Bojan Tadic <btadic@execom.eu>
2012-06-29 13:49:37 +0200

    All theme colors are difined in the domui.color.js

--- to.etc.domui/src/resources/themes/domui.color.js
+++ to.etc.domui/src/resources/themes/domui.color.js
@@ -42,4 +42,26 @@ even_bg="#eeeeee"; //greyish
 dt_rowhdr_bg="#98bbf3";
 dt_rowhdr_fg="#ffffff";
 
+//-- input bevel: darker grey / lighter grey
+bevel_up="#ABADB3";
+bevel_down="#BFCDD9";
+
+//-- hovel input bevel for a component: dark blue/light blue
+bevel_hover_up="#5794BF";
+bevel_hover_down="#D7E8F8";
+
+//-- button color
+button_bg="#6290E1";
+button_text_color="#ffffff";
+
+//-- Header colors
+header_bg="#6290E1";
+header_bg2="#ffffff";
+header_text_color="#ffffff";
+
+//-- Error colors (error panel)
+error_bg="#a9c5f1";
+
+//-- selected items background
+selected_bg="#ff9436";
 
--- to.etc.domui/src/resources/themes/domui/style.props.js
+++ to.etc.domui/src/resources/themes/domui/style.props.js
@@ -19,26 +19,4 @@ dpr_dis_last="THEME/dpr-last-dis.png.svg?w=20&h=20";
 defaultbutton_height=23;
 window_title_font_size='15px';
 
-//-- input bevel: darker grey / lighter grey
-bevel_up="#ABADB3";
-bevel_down="#BFCDD9";
-
-//-- hovel input bevel for a component: dark blue/light blue
-bevel_hover_up="#5794BF";
-bevel_hover_down="#D7E8F8";
-
-//-- button color
-button_bg="#6290E1";
-button_text_color="#ffffff";
-
-//-- Header colors
-header_bg="#6290E1";
-header_bg2="#ffffff";
-header_text_color="#ffffff";
-
-//-- Error colors (error panel)
-error_bg="#a9c5f1";
-
-//-- selected items background
-selected_bg="#ff9436";
 
