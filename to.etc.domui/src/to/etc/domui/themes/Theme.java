6c7c0eb42a0e029ef1e5f74544e2e6fc073fb080
Vladimir Mijic <vmijic@execom.eu>
2012-09-03 11:45:00 +0200

    Added proposals for icons for hand and digitally signed

--- to.etc.domui/src/to/etc/domui/themes/Theme.java
+++ to.etc.domui/src/to/etc/domui/themes/Theme.java
@@ -8,7 +8,7 @@ import to.etc.domui.dom.html.*;
  * Please keep in alphabetical order.
  *
  * FIXME: for btadic -> fill in all resources (search for '"THEME' in code, and replace with constant that is placed in here).
- * We would schedule that once when we agree upon this with jal in order to minimize conflicts. Currenlty there are 222 hits for searching on '"THEME/'
+ * We would schedule that once when we agree upon this with jal in order to minimize conflicts. Currently there are 222 hits for searching on '"THEME/'
  *
  * @author <a href="mailto:vmijic@execom.eu">Vladimir Mijic</a>
  * Created on 9 Nov 2011
@@ -35,6 +35,10 @@ public class Theme {
 	static public final String ICON_WARNING = "THEME/warning.png";
 	
 	static public final String ICON_ERROR = "THEME/mbx-error.png";
+
+	static public final String ICON_PAW = "THEME/paw.png";
+
+	static public final String ICON_SECURED = "THEME/secured.png";
 	
 	static public final String ICON_SUCCESS = "THEME/success.png";
 }
