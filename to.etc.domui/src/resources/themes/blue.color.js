b79acc5581ab4b24385e89c62d0da9dc01e18cd0
Bojan Tadic <btadic@execom.eu>
2012-10-17 11:00:20 +0200

    Work on fragments

--- to.etc.domui/src/resources/themes/blue.color.js
+++ to.etc.domui/src/resources/themes/blue.color.js
@@ -1,3 +1,5 @@
+header_bg="#7c7c7c";
+
 bg_color="#ffffff";
 bg2_color=util.color(bg_color).lume(0.3);
 body_image='bg-body-domui.png';
@@ -15,12 +17,20 @@ button_height=24;
 link_font_size="12px";
 link_color="#2200cc";	// dark blue
 
-//-- Selects and highlights ---
-selected_bg="#bed6f8";	// blue
 highlight_bg="#ffbb43";	// row select: hard orange
 highlight2_bg="#ffca6f"; // secondary highlight: salmon
-cellhighlight_bg="#67267f";	// cell select: purple
-cellhighlightcolor_bg="#ffffff";	// link color of cell select: white
+
+//-- Header colors
+data_tbl_header_bg="#7c7c7c";
+data_tbl_header_text_color="#ffffff";
+data_tbl_header_bg_img=""; //-- Table header doesn't have background image
+
+//-- Selects and highlights ---
+data_tbl_selected_bg="#bed6f8";	// blue
+data_tbl_even_row_bg="#e8e8e8"; //greyish
+data_tbl_cell_highlight_bg="#67267f";	// cell select: purple
+data_tbl_cell_highlight_link_color_bg="#ffffff";	// link color of cell select: white
+data_tbl_font_size = "12px";
 
 readonly_bg="transparent"; 
 readonly_border="#EEEEEF"; // bit darker grey
@@ -39,7 +49,6 @@ info_fg="blue";
 info_border="blue";
 
 // row odd/even
-even_row_bgcol="#e8e8e8"; //greyish
 dt_rowhdr_bg="#98bbf3";
 dt_rowhdr_fg="#ffffff";
 
@@ -53,13 +62,7 @@ bevel_hover_down="#D7E8F8";
 
 //-- button color
 button_bg="#6290E1";
-button_text_color="#ffffff";
-
-//-- Header colors
-header_bg="#7c7c7c";
-header_bg2="#ffffff";
-header_text_color="#ffffff";
-header_bg_img=""; //-- Table header doesn't have background image   
+button_text_color="#ffffff";   
 
 //-- Caption
 caption_separator="hr-caption.png";
@@ -82,7 +85,7 @@ caption_bg="#f0810a"; //Main orange
 
 //-- Bulk upload
 upl_runing=highlight_bg;
-upl_loading=cellhighlight_bg;/* Icon definition for DomUI icons */
+upl_loading=data_tbl_cell_highlight_bg;/* Icon definition for DomUI icons */
 
 /*** Data Pager buttons ***/
 nav_btn_first_s = 'nav-first-s.png';
