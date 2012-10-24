23fef7a7f6bca38d2e90cc9e8a68cdca956f4d1f
Frits Jalvingh <jal@etc.to>
2012-10-24 17:33:31 +0200

    Minor fixes.

--- to.etc.domui/src/resources/themes/blue/style.props.js
+++ to.etc.domui/src/resources/themes/blue/style.props.js
@@ -2,12 +2,14 @@
  * "blue" stylesheet properties.
  */
 font_family='Verdana, Geneva, "DejaVu Sans", sans-serif';
+//font_family='Verdana, Tahoma, helvetica, sans-serif';
 font_size="11px";
 hdr_font_size="14px";
 
 fixed_font_family='Courier New, Courier, monospace';
 fixed_font_size="11px";
 
+//special_font = "Verdana, Tahoma, helvetica, sans-serif";
 special_font = "Arial, Helvetica, sans-serif";
 //special_font = "Biolinum2, Courier, serif";
 special_font_size = "13px";
