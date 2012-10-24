046b77894ef1df26a411ae2ca808936d920632c9
Frits Jalvingh <jal@etc.to>
2012-10-24 17:41:42 +0200

    Make both style properties equal

--- to.etc.domui/src/resources/themes/domui/style.props.js
+++ to.etc.domui/src/resources/themes/domui/style.props.js
@@ -1,12 +1,21 @@
 /*
- * "domui" stylesheet properties.
+ * "blue" stylesheet properties.
  */
 font_family='Verdana, Geneva, "DejaVu Sans", sans-serif';
+//font_family='Verdana, Tahoma, helvetica, sans-serif';
 font_size="11px";
 hdr_font_size="14px";
 
 fixed_font_family='Courier New, Courier, monospace';
 fixed_font_size="11px";
+
+//special_font = "Verdana, Tahoma, helvetica, sans-serif";
+special_font = "Arial, Helvetica, sans-serif";
+//special_font = "Biolinum2, Courier, serif";
+special_font_size = "13px";
+
+button_font_family='Verdana, Geneva, "DejaVu Sans", sans-serif';
+button_font_size="11px";
 
 defaultbutton_height=23;
 window_title_font_size='15px';
