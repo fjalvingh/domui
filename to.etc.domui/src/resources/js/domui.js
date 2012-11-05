fb32c0a78469002fc076b5cff27ddda5000ecee8
Frits Jalvingh <jal@etc.to>
2012-11-05 18:29:35 +0100

    Fix bug in focusing components in delta on Chrome. Allow a Window to start a new "default focused component" inside it's content area. This fixes the problem that popups do not have focused input.

--- to.etc.domui/src/resources/js/domui.js
+++ to.etc.domui/src/resources/js/domui.js
@@ -1221,12 +1221,16 @@ var WebUI = {
 	
 	focus : function(id) {
 		var n = document.getElementById(id);
-		try{
-			if (n)
-				setTimeout(function() { try { n.focus();} catch (e) { /*just ignore */ } }, 100); //Due to IE bug, we need to set focus on timeout :( See http://www.mkyong.com/javascript/focus-is-not-working-in-ie-solution/    				
-				//n.focus();
-		} catch (e) {
-			//just ignore
+		if(n) {
+			if($.browser.isIE) {
+				setTimeout(function() { try { n.focus();} catch (e) { /*just ignore */ } }, 100); //Due to IE bug, we need to set focus on timeout :( See http://www.mkyong.com/javascript/focus-is-not-working-in-ie-solution/
+			} else {
+				try {
+					n.focus();
+				} catch(e) {
+					//-- ignore
+				}
+			}
 		}
 	},
 	
