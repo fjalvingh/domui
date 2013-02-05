5b5dca622d0890d0d80e618815bb31e0dfa30047
Frits Jalvingh <jal@etc.to>
2013-02-05 22:31:08 +0100

    Add root path

---
+++ to.etc.domui/src/to/etc/domui/logic/paths/PathRoot.java
@@ -0,0 +1,16 @@
+package to.etc.domui.logic.paths;
+
+import javax.annotation.*;
+
+public class PathRoot<T> implements IPropertyPathElement<T> {
+	private T m_instance;
+
+	@Override
+	@Nonnull
+	public T getInstance() {
+		return m_instance;
+	}
+
+	@Override
+	public void appendPath(@Nonnull StringBuilder sb) {}
+}
