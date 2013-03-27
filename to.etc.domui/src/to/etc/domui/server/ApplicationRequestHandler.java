dba7dc66cdfc6aab1bd94954dbb472e3c502ee3d
Frits Jalvingh <jal@etc.to>
2013-03-27 11:54:08 +0100

    Replace system.out with logger.

--- to.etc.domui/src/to/etc/domui/server/ApplicationRequestHandler.java
+++ to.etc.domui/src/to/etc/domui/server/ApplicationRequestHandler.java
@@ -180,7 +180,7 @@ public class ApplicationRequestHandler implements IFilterRequestHandler {
 				if(m_application.getAutoRefreshPollInterval() <= 0) {
 					generateExpired(ctx, Msgs.BUNDLE.getString(Msgs.S_EXPIRED));
 				} else {
-					System.out.println("DEBUG: Not sending expired message because autorefresh is ON for " + cid);
+					LOG.info("Not sending expired message because autorefresh is ON for " + cid);
 				}
 			}
 
@@ -271,7 +271,7 @@ public class ApplicationRequestHandler implements IFilterRequestHandler {
 						if(m_application.getAutoRefreshPollInterval() <= 0) {
 							generateExpired(ctx, Msgs.BUNDLE.getString(Msgs.S_EXPIRED));
 						} else
-							System.out.println("DEBUG: Not sending expired message because autorefresh is ON for " + cid);
+							LOG.info("Not sending expired message because autorefresh is ON for " + cid);
 					}
 					return;
 				}
