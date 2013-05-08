8c54df85399acd362535c5ab83bb79a79c14bcad
Frits Jalvingh <jal@etc.to>
2013-05-08 19:13:12 +0200

    Start with special component binding to handle errors during binding properly.

---
+++ to.etc.domui/src/to/etc/domui/databinding/ComponentObserverSupport.java
@@ -0,0 +1,19 @@
+package to.etc.domui.databinding;
+
+import javax.annotation.*;
+
+/**
+ * This is a special observable handler for DomUI components. This observer handles
+ * property changes in such a way that errors in the validation and conversion of the
+ * properties are handled differently for binding than for normal "getValue" calls.
+ *
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on May 8, 2013
+ */
+public class ComponentObserverSupport<C> extends ObserverSupport<C> {
+	public ComponentObserverSupport(@Nonnull C instance) {
+		super(instance);
+	}
+
+
+}
