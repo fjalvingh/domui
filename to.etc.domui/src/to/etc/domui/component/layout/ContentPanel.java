510359489d41070ab31218583d6fa512acca2a70
Frits Jalvingh <jal@etc.to>
2013-07-11 18:54:19 +0200

    Add content panel which has padding

---
+++ to.etc.domui/src/to/etc/domui/component/layout/ContentPanel.java
@@ -0,0 +1,15 @@
+package to.etc.domui.component.layout;
+
+import to.etc.domui.dom.html.*;
+
+/**
+ * A simple div acting as a content panel, with margins all around.
+ *
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on Jul 10, 2013
+ */
+public class ContentPanel extends Div {
+	public ContentPanel() {
+		setCssClass("ui-cpnl");
+	}
+}
