2e4622e3edd18645acab032164edc517ba23f4f4
Frits Jalvingh <jal@etc.to>
2013-10-18 17:55:27 +0200

    Merge simple additions from formbuilder branch

--- to.etc.domui/src/resources/js/domui.js
+++ to.etc.domui/src/resources/js/domui.js
@@ -509,6 +509,44 @@ $(window).bind('beforeunload', function() {
 	};
 })(jQuery);
 
+(function($) {
+	if($.browser.msie && $.browser.majorVersion < 10) {
+		$.dbg = function(a,b,c,d,e) {
+			if(window.console == undefined)
+				return;
+			switch(arguments.length) {
+			default:
+				window.console.log(a);
+				return;
+			case 2:
+				window.console.log(a,b);
+				return;
+			case 3:
+				window.console.log(a,b,c);
+				return;
+			case 4:
+				window.console.log(a,b,c,d);
+				return;
+			case 5:
+				window.console.log(a,b,c,d,e);
+				return;
+			}
+		};
+	} else if(window.console != undefined) {
+		if(window.console.debug != undefined) {
+			$.dbg = function() {
+				window.console.debug.apply(window.console, arguments);
+			};
+		} else if(window.console.log != undefined) {
+			$.dbg = function() {
+				window.console.log.apply(window.console, arguments);
+			};
+		}
+	} else {
+		$.dbg = function() {};
+	}
+})(jQuery);
+
 (function ($) {
 	$.fn.doStretch = function () {
 		return this.each(function () {
