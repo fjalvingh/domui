0045d9664772fe94a7d7a9b1e3340ca3077e0921
Vladimir Mijic <vmijic@execom.eu>
2013-12-17 16:50:47 +0100

    Added more details on usage for added method for callable statement

--- to.etc.webapp.core/test/to/etc/test/webapp/query/TestJdbcUtil.java
+++ to.etc.webapp.core/test/to/etc/test/webapp/query/TestJdbcUtil.java
@@ -23,13 +23,29 @@ public class TestJdbcUtil {
 	}
 
 	@Test
-	public void testCallableStatement() throws Exception {
+	public void testUpdatingCallableStatement() throws Exception {
 		//FIXME: currently this test depends on certain table, we have to find solution for this...
 		String sql = "begin INSERT INTO RED_PARAMETERS (rpr_module, rpr_name, rpr_char1) VALUES (?,?,?) RETURNING rpr_id INTO ? ; end;";
 		JdbcOutParam<Long> idParam = new JdbcOutParam<Long>(Long.class);
-		if(JdbcUtil.executeCallableStatement(m_ds.getConnection(), sql, "test", "test", "test", idParam)) {
+		if(JdbcUtil.executeUpdatingCallableStatement(m_ds.getConnection(), sql, "test", "test", "test", idParam)) {
 			Assert.assertNotNull(idParam.getValue());
 		}
 		m_dc.rollback();
+	}
+
+	@Test
+	public void testInvalidUpdatingCallableStatement() throws Exception {
+		boolean exFound = false;
+		String sql = "INSERT INTO RED_PARAMETERS (rpr_module, rpr_name, rpr_char1) VALUES (?,?,?) RETURNING rpr_id INTO ?;";
+		JdbcOutParam<Long> idParam = new JdbcOutParam<Long>(Long.class);
+		try {
+			if(JdbcUtil.executeUpdatingCallableStatement(m_ds.getConnection(), sql, "test", "test", "test", idParam)) {
+				Assert.assertNotNull(idParam.getValue());
+			}
+		} catch(IllegalArgumentException ex) {
+			exFound = true;
+		}
+		m_dc.rollback();
+		Assert.assertTrue(exFound);
 	}
 }
