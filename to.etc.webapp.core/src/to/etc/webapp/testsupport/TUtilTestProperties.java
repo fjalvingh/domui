9e6ddf452acfe5f586ad32a66c02ef88332d2998
Frits Jalvingh <jal@etc.to>
2014-01-29 16:47:08 +0100

    Add code to initialize standard logging (slf4j) using a logback.xml file that is either default or configurable.

--- to.etc.webapp.core/src/to/etc/webapp/testsupport/TUtilTestProperties.java
+++ to.etc.webapp.core/src/to/etc/webapp/testsupport/TUtilTestProperties.java
@@ -7,9 +7,16 @@ import java.util.*;
 import java.util.Date;
 
 import javax.annotation.*;
+import javax.annotation.concurrent.*;
 import javax.sql.*;
 
 import org.junit.*;
+import org.slf4j.*;
+import org.slf4j.bridge.*;
+
+import ch.qos.logback.classic.*;
+import ch.qos.logback.classic.joran.*;
+import ch.qos.logback.core.util.*;
 
 import to.etc.dbpool.*;
 import to.etc.dbutil.*;
@@ -307,6 +314,68 @@ public class TUtilTestProperties {
 			return;
 		m_connectionPool.setCommitDisabled(on);
 	}
+
+
+	/*--------------------------------------------------------------*/
+	/*	CODING:	Initialize slf4j logger.							*/
+	/*--------------------------------------------------------------*/
+
+	@GuardedBy("class")
+	static private boolean m_loggingInitialized;
+
+	static synchronized public void initLogging() {
+		if(m_loggingInitialized)
+			return;
+		m_loggingInitialized = true;
+		String logconfig = getString("logconfig", null);
+		if(logconfig == null) {
+			logconfig = System.getProperty("domui.logconfig");
+		}
+		InputStream logStream = findLogConfig(logconfig);
+		try {
+			if(logStream != null) {
+				JoranConfigurator jc = new JoranConfigurator();
+				LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+				jc.setContext(lc);
+				lc.reset();
+				jc.getStatusManager().clear();
+				jc.doConfigure(logStream);
+				System.out.println("TUtilTestProperties: logging configured.");
+				StatusPrinter.printInCaseOfErrorsOrWarnings(lc);
+				SLF4JBridgeHandler.install();
+			}
+
+		} catch(Exception x) {
+			System.err.println("TUtilTestProperties: logging initialization failed with " + x);
+		}
+	}
+
+	@Nullable
+	static private InputStream findLogConfig(@Nonnull String logconfig) {
+		if(logconfig != null) {
+			//-- Try to find this as a class-relative resource;
+			if(!logconfig.startsWith("/")) {
+				InputStream is = TUtilTestProperties.class.getResourceAsStream("/" + logconfig);
+				if(is != null) {
+					System.out.println("TUtilTestProperties: using user-specified logback config file from classpath-resource " + logconfig);
+					return is;
+				}
+			}
+
+			try {
+				File f = new File(logconfig);
+				if(f.exists() && f.isFile()) {
+					System.out.println("TUtilTestProperties: using logback logging configuration file " + f.getAbsolutePath());
+					return new FileInputStream(f);
+				}
+			} catch(Exception x) {}
+		}
+		InputStream is = TUtilTestProperties.class.getResourceAsStream("logback.xml");
+		if(is != null)
+			System.out.println("TUtilTestProperties: using internal logback.xml");
+		return is;
+	}
+
 
 	/*--------------------------------------------------------------*/
 	/*	CODING:	Test logging using a LogSink.						*/
