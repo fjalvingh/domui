23ba0ce6cb24944eb64aa6668a7df5c39fe62139
Frits Jalvingh <jal@etc.to>
2014-02-11 18:10:07 +0100

    Fix failed JUnit test in DomUI: style sheet expander

--- to.etc.domui/src/resources/themes/domui/style.props.js
+++ to.etc.domui/src/resources/themes/domui/style.props.js
@@ -1,6 +1,8 @@
 // inherit('$themes/coretheme');
 font_family='Verdana, Tahoma, Arial, Bitstream Vera, Helvetica, sans-serif';
 font_size='12px';
+hdr_font_size="14px";
 
 window_title_font_size='15px';
+defaultbutton_height=22;
 
