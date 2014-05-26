024ad62b4857d07e04c5cf3c3cee548d3a2cc7d6
Bojan Tadic <btadic@execom.eu>
2014-05-26 10:18:28 +0200

    Removing date checker method duplicates from domui.js

--- to.etc.domui/src/resources/js/domui.js
+++ to.etc.domui/src/resources/js/domui.js
@@ -1592,47 +1592,6 @@ $.extend(WebUI, {
 		WebUI.dateInputRepairValueIn(c);
 	},
 
-	dateInputRepairValueIn : function(c) {
-		if(!c)
-			return;
-		var val = c.value;
-
-		if(!val || val.length == 0) // Nothing to see here, please move on.
-			return;
-		Calendar.__init();
-
-		// -- 20130425 jal if this is a date+time thing the value will hold space-separated time, so make sure to split
-		// it;
-		var pos = val.indexOf(' ');
-		var timeval = null;
-		if(pos != -1) {
-			// -- Split into trimmed time part and val = only date
-			timeval = $.trim(val.substring(pos + 1));
-			val = $.trim(val.substring(0, pos));
-		}
-
-		// -- Try to decode then reformat the date input
-		var fmt = Calendar._TT["DEF_DATE_FORMAT"];
-		try {
-			var separatorsCount = WebUI.countSeparators(val)
-			if(separatorsCount < 2) {
-				val = WebUI.insertDateSeparators(val, fmt, separatorsCount);
-				var res = Date.parseDate(val, fmt);
-				c.value = res.print(fmt);
-			} else {
-				try{
-					var resultOfConversion = WebUI.parsingOfFormat(val, fmt);
-					var res = Date.parseDate(resultOfConversion, fmt);
-					c.value = res.print(fmt);
-				}catch(x){
-					Date.parseDate(val, fmt);
-				}
-			}
-		} catch(x) {
-			alert(Calendar._TT["INVALID"]);
-		}
-	},
-
 	/**
 	 * Function that checks is format valid after check that input has separators.
 	 */
@@ -1709,76 +1668,6 @@ $.extend(WebUI, {
 		var NEEDED_CHARACTER_YEAR = "20";
 
 		return result += NEEDED_CHARACTER_YEAR + inputValue;
-	},
-
-	/**
-	 * Count of separator chars (anything else than letters and/or digits).
-	 */
-	countSeparators: function(str) {
-		var count = 0;
-		for(var i = str.length; --i >= 0;) {
-			if(WebUI.isSeparator(str.charAt(i)))
-				count++;
-		}
-		return count;
-	},
-
-	/**
-	 * Returns T if char is anything else than letters and/or digits.
-	 */
-	isSeparator: function(c) {
-		return !(( c >= 'A' && c <= 'Z') || (c >='a' && c <= 'z') || (c >= '0' && c <= '9'));
-	},
-
-	insertDateSeparators: function(str, fmt, separatorsCount) {
-		var b = fmt.match(/%./g); // Split format items
-		var len = str.length;
-		var ylen;
-		if(len == 8)
-			ylen = 4;
-		else if(len == 6)
-			ylen = 2;
-		else if(len >= 3 && len <= 5)
-			ylen = 0;
-		else
-			throw "date invalid";
-		// dd-mm dd/mm case - ignore existing separator
-		if (separatorsCount == 1) {
-			var index = 0;
-			while (! WebUI.isSeparator(str.charAt(index))) {
-				index++;
-				if (index>len-1) {
-					throw "invalid state";
-				}
-			}
-			str = str.substring(0, index) + '-' +  str.substring(index+1);
-		}
-		//-- Edit the string according to the pattern,
-		var res = "";
-		for(var fix= 0; fix < b.length; fix++) {
-			if(res.length != 0 && str.length != 0 )
-				res = res + '-';				// Just a random separator.
-			switch(b[fix]) {
-				default:
-					throw "date invalid";
-				case "%d":
-		    	case "%e":
-			    case "%m":
-		    		// Pre-existing dash separator or 2-digit day or month. Copy.
-			    	var dashIndex = str.indexOf('-');
-			    	var index = dashIndex == -1 ? 2 : dashIndex;
-			    	var indexNext = dashIndex == -1 ? 2 : dashIndex + 1;
-		    		res += str.substring(0, index);
-		    		str = str.substring(indexNext);
-		    		break;
-			    case '%y': case '%Y':
-			    	//-- 2- or 4 digit year,
-		    		res += str.substring(0, ylen);
-		    		str = str.substring(ylen);
-			    	break;
-			}
-		}
-		return res;
 	},
 
 	/**
