7d8bcff33daa735d88def2b1ffe08dd7d45a781c
Ben Schoen <ben.schoen@itris.nl>
2014-06-20 09:54:35 +0200

    Review comments from Marc processed. Field and method names adjusted.

--- to.etc.alg/src/to/etc/util/ExceptionClassifier.java
+++ to.etc.alg/src/to/etc/util/ExceptionClassifier.java
@@ -6,7 +6,7 @@ import java.util.*;
 import javax.annotation.*;
 
 /**
- * This determines whether an exception should be logged or not.
+ * This determines whether an exception is severe or not.
  *
  * There are roughly two classes of exceptions:
  * <ol>
@@ -32,32 +32,32 @@ import javax.annotation.*;
 public final class ExceptionClassifier {
 
 	@Nonnull
-	private static final Map<String, Boolean> m_severeExceptions = new HashMap<String, Boolean>(); //Boolean.TRUE means that it is severe, Boolean.FALSE means it is not severe.
+	private static final Map<String, Boolean> m_knownExceptions = new HashMap<String, Boolean>(); //Boolean.TRUE means that it is severe, Boolean.FALSE means it is not severe.
 
 	static {
-		m_severeExceptions.put("ORA-02292: integrity constraint", Boolean.FALSE);											// Violation of integrity constraint(s), shown on screen, not severe.
-		m_severeExceptions.put("ORA-20000: Gegevens zijn gewijzigd door een andere gebruiker", Boolean.FALSE);				// Concurrency exception, shown on the screen, not severe.
+		m_knownExceptions.put("ORA-02292: integrity constraint", Boolean.FALSE);											// Violation of integrity constraint(s), shown on screen, not severe.
+		m_knownExceptions.put("ORA-20000: Gegevens zijn gewijzigd door een andere gebruiker", Boolean.FALSE);				// Concurrency exception, shown on the screen, not severe.
 		// TODO: check and test this error message on 5.0
 		//m_severeExceptions.put("ClientAbortException:  java.net.SocketException: Connection reset", Boolean.FALSE);			// Exception when planboard is closed before it's fully loaded. Not severe.
-		m_severeExceptions.put("ORA-20023: tda_general.check_beperking: Combinatie <B>Elementcode:</B>:", Boolean.FALSE);	// Misconfiguration of elementcode/werksoort/fonds combination, shown on screen, not severe.
-		m_severeExceptions.put("De PDA is niet toegewezen aan een persoon", Boolean.FALSE);									// Thrown when PDA is reconnected to another environment, not severe.
+		m_knownExceptions.put("ORA-20023: tda_general.check_beperking: Combinatie <B>Elementcode:</B>:", Boolean.FALSE);	// Misconfiguration of elementcode/werksoort/fonds combination, shown on screen, not severe.
+		m_knownExceptions.put("De PDA is niet toegewezen aan een persoon", Boolean.FALSE);									// Thrown when PDA is reconnected to another environment, not severe.
 
-		m_severeExceptions.put("ORA-12899: value too large for column", Boolean.TRUE);
+		m_knownExceptions.put("ORA-12899: value too large for column", Boolean.TRUE);
 	}
 
 	@Nonnull
 	public static boolean isSevereException(@Nonnull Throwable e) {
 		List<Throwable> thrownExceptions = new ArrayList<Throwable>();
 
-		createListOfExceptions(e, thrownExceptions);
+		addAcceptionsToList(e, thrownExceptions);
 
 		boolean foundUnsevereException = false;
 		for(Throwable t : thrownExceptions) {
 			String message = t.getMessage();
 			if(message != null) {
-				for(String exceptionMsg : m_severeExceptions.keySet()) {
+				for(String exceptionMsg : m_knownExceptions.keySet()) {
 					if(message.startsWith(exceptionMsg)) {
-						if(m_severeExceptions.get(exceptionMsg).booleanValue()) {
+						if(m_knownExceptions.get(exceptionMsg).booleanValue()) {
 							return true;
 						} else {
 							foundUnsevereException = true;
@@ -69,18 +69,18 @@ public final class ExceptionClassifier {
 		return foundUnsevereException ? false : true;
 	}
 
-	private static void createListOfExceptions(@Nonnull Throwable e, @Nonnull List<Throwable> thrownExceptions) {
+	private static void addAcceptionsToList(@Nonnull Throwable e, @Nonnull List<Throwable> thrownExceptions) {
 		thrownExceptions.add(e);
 		if(e instanceof SQLException) {
 			SQLException sqle = ((SQLException) e).getNextException();
 			if(sqle != null) {
-				createListOfExceptions(sqle, thrownExceptions);
+				addAcceptionsToList(sqle, thrownExceptions);
 			}
 		}
 
 		Throwable cause = e.getCause();
 		if(cause != null) {
-			createListOfExceptions(cause, thrownExceptions);
+			addAcceptionsToList(cause, thrownExceptions);
 		}
 	}
 }
