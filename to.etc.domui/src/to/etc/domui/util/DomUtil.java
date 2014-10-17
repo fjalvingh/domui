6c3e167a9cec0bea108f08c08f8aa6ba9a49a07d
Bojan Tadic <btadic@execom.eu>
2014-10-17 18:21:37 +0200

    Reverting change that is causing 18 failed tests.

--- to.etc.domui/src/to/etc/domui/util/DomUtil.java
+++ to.etc.domui/src/to/etc/domui/util/DomUtil.java
@@ -930,7 +930,7 @@ final public class DomUtil {
 			if(c == ' ')
 				sb.append('_');
 			else if(Character.isLetterOrDigit(c) || c == '_' || c == '-')
-				sb.append(Character.toLowerCase(c));
+				sb.append(c);
 		}
 
 		return sb.toString();
