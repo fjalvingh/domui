49be745c7d6a9d37b785a7126d79e0100f1ffb3f
Frits Jalvingh <jal@etc.to>
2014-11-10 19:49:28 +0100

    Flesh out bare bones of duplicator

--- to.etc.domui/src/to/etc/domui/component/meta/MetaDuplicator.java
+++ to.etc.domui/src/to/etc/domui/component/meta/MetaDuplicator.java
@@ -1,5 +1,6 @@
 package to.etc.domui.component.meta;
 
+import java.lang.reflect.*;
 import java.util.*;
 
 import javax.annotation.*;
@@ -25,6 +26,8 @@ public class MetaDuplicator<T> {
 
 	@Nonnull
 	private Map<Object, Object> m_old2newmap = new HashMap<>();
+
+	private int m_copies;
 
 	public MetaDuplicator(T source) {
 		m_source = source;
@@ -95,43 +98,101 @@ public class MetaDuplicator<T> {
 
 	public T build() throws Exception {
 		StringBuilder sb = new StringBuilder();
-		ClassMetaModel cmm = MetaManager.findClassMeta(m_source.getClass());
-
-		T copy = (T) cmm.getClass().newInstance();
-		for(PropertyMetaModel< ? > pmm : cmm.getProperties()) {
-			copyProperty(copy, m_source, pmm, sb);
-		}
+		T copy = cloneInstance(m_source, sb);
 		return copy;
 	}
 
-	private <V> void copyProperty(T copy, T source, PropertyMetaModel<V> pmm, StringBuilder sb) {
+	private <I> I cloneInstance(I source, StringBuilder sb) throws InstantiationException, IllegalAccessException, Exception {
+		if(++m_copies > 100)
+			throw new IllegalStateException("Too many copies");
+		ClassMetaModel cmm = MetaManager.findClassMeta(source.getClass());
+		I copy = (I) cmm.getClass().newInstance();
+		copyProperties(copy, source, sb, cmm);
+		return copy;
+	}
+
+	private <I> void copyProperties(I copy, I source, StringBuilder sb, ClassMetaModel cmm) throws Exception {
+		for(PropertyMetaModel< ? > pmm : cmm.getProperties()) {
+			copyProperty(copy, m_source, pmm, sb);
+		}
+	}
+
+	private <I, V> void copyProperty(I copy, I source, PropertyMetaModel<V> pmm, StringBuilder sb) throws Exception {
 		if(pmm.getReadOnly() == YesNoType.YES)
 			return;
 		int len = sb.length();
-		if(len > 0) {
-			sb.append('.');
+		try {
+			int len2 = len;
+			if(len > 0) {
+				sb.append('.');
+				len2++;
+			}
+
+			sb.append("*");
+			String all = sb.toString();
+
+			sb.setLength(len2);
+			sb.append(pmm.getName());
+			String prop = sb.toString();
+
+			Mode mode = m_modeMap.get(prop);
+			if(mode == null) {
+				mode = m_modeMap.get(all);
+				if(mode == null)
+					mode = m_defaultMode;
+			}
+			if(mode == Mode.IGNORE)
+				return;
+
+			//-- Get value, and copy and exit immediately on null.
+			V srcvalue = pmm.getValue(source);
+			if(srcvalue == null) {
+				pmm.setValue(copy, null);
+				return;
+			}
+
+			if(List.class.isAssignableFrom(pmm.getActualType())) {
+				copyListProperty(copy, source, pmm, sb, mode);
+			} else if(isUncopyable(pmm) || mode == Mode.SHALLOW) {
+				pmm.setValue(copy, srcvalue);
+			} else {
+				V dstvalue = (V) m_old2newmap.get(srcvalue);
+				if(null != dstvalue) {
+					//-- Must create new instance
+					dstvalue = cloneInstance(srcvalue, sb);
+					m_old2newmap.put(srcvalue, dstvalue);
+				}
+				pmm.setValue(copy, dstvalue);
+			}
+		} finally {
+			sb.setLength(len);
 		}
-		sb.append("*");
-		String all = sb.toString();
-
-
-		if(List.class.isAssignableFrom(pmm.getActualType())) {
-			copyListProperty(copy, source, pmm, sb);
-		} else if(isUncopyable(pmm)) {
-			copyValue(copy, source, pmm, sb);
-		} else {
-
-		}
-
-		sb.setLength(len);
 	}
+
+	private Class< ? >[] UNCOPYABLE = new Class< ? >[]{Date.class, String.class,};
 
 	private boolean isUncopyable(PropertyMetaModel< ? > pmm) {
+		Class<?> clz = pmm.getActualType();
+		if(clz.isPrimitive())
+			return true;
+		if(pmm.getRelationType() == PropertyRelationType.UP)
+			return false;
+		try {
+			Constructor< ? > cons = clz.getConstructor();
+			if(!Modifier.isPublic(cons.getModifiers()))
+				return true;
+		} catch(Exception x) {
+			return true;							// No default constructor -> uncopyable
+		}
 
+		for(Class< ? > uc : UNCOPYABLE) {
+			if(uc.isAssignableFrom(clz))
+				return true;
+		}
+		return false;
 	}
 
-
-	private <V> void copyListProperty(T copy, T source, PropertyMetaModel<V> pmm, StringBuilder sb) {
+	private <I, V> void copyListProperty(I copy, I source, PropertyMetaModel<V> pmm, StringBuilder sb, Mode mode) {
 
 	}
 }
