17802fa6ea84e94c3a6dc76f3d4e5399a52f79e7
Frits Jalvingh <jal@etc.to>
2014-12-03 18:41:39 +0100

    Fix the very bad seconds converter.

--- to.etc.domui/src/to/etc/domui/converter/OldSecondDurationConverter.java
+++ to.etc.domui/src/to/etc/domui/converter/OldSecondDurationConverter.java
@@ -25,12 +25,12 @@
 package to.etc.domui.converter;
 
 import to.etc.domui.trouble.*;
+import to.etc.domui.util.*;
+import to.etc.util.*;
 
 import java.util.*;
 
-public class SecondDurationConverter implements IConverter<Long> {
-	private static final MsDurationConverter INSTANCE = new MsDurationConverter();
-
+public class OldSecondDurationConverter implements IConverter<Long> {
 	static private final long DAYS = 24 * 60 * 60;
 
 	static private final long HOURS = 60 * 60;
@@ -39,20 +39,47 @@ public class SecondDurationConverter implements IConverter<Long> {
 	public String convertObjectToString(Locale loc, Long in) throws UIException {
 		if(in == null)
 			return "";
-		return INSTANCE.convertObjectToString(loc, Long.valueOf(in.longValue() * 1000));
+		//		if(!(in instanceof Number))
+		//			throw new IllegalStateException("Type must extend Number for this converter");
+		long dlt = ((Number) in).longValue();
+		//		boolean	sp = false;
+		StringBuilder sb = new StringBuilder(20);
+		if(dlt > DAYS) {
+			sb.append(Long.toString(dlt / DAYS));
+			sb.append("D ");
+			dlt %= DAYS;
+		}
+
+		sb.append(Long.toString(dlt / HOURS)); // #of hours (0..23)
+		dlt %= HOURS;
+		sb.append(':');
+		sb.append(StringTool.intToStr((int) (dlt / 60), 10, 2));
+		dlt %= 60;
+		if(dlt > 0) {
+			sb.append(':');
+			sb.append(StringTool.intToStr((int) dlt, 10, 2));
+		}
+		return sb.toString();
 	}
 
 	/**
 	 * Convert a duration in the format: [xD] hh:mm[:ss] into a duration in seconds.
-	 * @see to.etc.domui.converter.IConverter#convertStringToObject(java.util.Locale, java.lang.String)
+	 * @see IConverter#convertStringToObject(java.util.Locale, String)
 	 */
 	@Override
 	public Long convertStringToObject(Locale loc, String input) throws UIException {
 		if(input == null)
 			return null;
-		Long res = INSTANCE.convertStringToObject(loc, input);
-		if(res == null)
-			return res;
-		return Long.valueOf(res.longValue() / 1000);
+		input = input.trim();
+		if(input.length() == 0)
+			return null;
+		try {
+			MiniScanner s = new MiniScanner();
+			return Long.valueOf(s.scanDuration(input));
+		} catch(ValidationException x) {
+			throw x;
+		} catch(Exception x) {
+			throw new ValidationException(Msgs.V_INVALID_DATE);
+		}
 	}
 }
