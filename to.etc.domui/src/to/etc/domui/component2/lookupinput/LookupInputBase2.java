e29740aee4ff0adea97047039e48390280b55583
Frits Jalvingh <jal@etc.to>
2015-01-02 18:46:17 +0100

    Bugfix: lookupInput2 did not obey root criteria on search dialog.

--- to.etc.domui/src/to/etc/domui/component2/lookupinput/LookupInputBase2.java
+++ to.etc.domui/src/to/etc/domui/component2/lookupinput/LookupInputBase2.java
@@ -186,6 +186,7 @@ abstract public class LookupInputBase2<QT, OT> extends Div implements IControl<O
 
 	public LookupInputBase2(@Nonnull ITableModelFactory<QT, OT> modelFactory, @Nonnull QCriteria<QT> rootCriteria, @Nonnull Class<OT> resultClass) {
 		this(modelFactory, DomUtil.nullChecked(rootCriteria.getBaseClass()), resultClass, (ClassMetaModel) null, (ClassMetaModel) null);
+		m_rootCriteria = rootCriteria;
 	}
 
 	public LookupInputBase2(@Nonnull ITableModelFactory<QT, OT> modelFactory, @Nonnull Class<QT> queryClass, @Nonnull Class<OT> resultClass, @Nullable ClassMetaModel queryMetaModel,
