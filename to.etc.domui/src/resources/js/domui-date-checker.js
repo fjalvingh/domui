72c6469864037fea44d8af4bd6d5076153153658
Bojan Tadic <btadic@execom.eu>
2015-01-26 09:47:10 +0100

    Fixing date checker to work with supported date and time formats.

--- to.etc.domui/src/resources/js/domui-date-checker.js
+++ to.etc.domui/src/resources/js/domui-date-checker.js
@@ -87,15 +87,27 @@ $.extend(WebUI, {
 	
 	dateInputRepairTimeValue : function(val) {
 		var fmt = Calendar._TT["DEF_TIME_FORMAT"];
-		var timeSeparator = Calendar._TT["TIME_SEPARATOR"];
-		if(val.indexOf(timeSeparator) < 0){
-			//add time separator before specified minutes
-			var placeForSeparator = val.length - 2;
-			val = [val.slice(0, placeForSeparator), timeSeparator, val.slice(placeForSeparator)].join('');
-		}
+		var tempSep = "~";
+		var count = WebUI.getTimeSeparatorCount(val);
+		switch(count) {
+			default:
+				throw "time has multiple separators";
+			case 0:
+				//add time separator before specified minutes
+				var placeForSeparator = val.length - 2;
+				val = [val.slice(0, placeForSeparator), tempSep, val.slice(placeForSeparator)].join('');
+			case 1:
+				var re = new RegExp("[" + Calendar._TT["TIME_SEPARATOR"] + "]","g");
+				val = val.replace(re, '~');
+			}
 		var dummyDate = new Date();
-		dummyDate.setHours(val.split(timeSeparator)[0], val.split(timeSeparator)[1], 0, 0);
+		dummyDate.setHours(val.split(tempSep)[0], val.split(tempSep)[1], 0, 0);
 		return dummyDate.print(fmt);
+	},
+	
+	getTimeSeparatorCount : function(time){
+		var supportedTimeSeparators = Calendar._TT["TIME_SEPARATOR"];
+		return (time.match(new RegExp("[" + supportedTimeSeparators + "]", "g")) || []).length;
 	},
 	
 	dateInputRepairDateTimeValue : function(val) {
