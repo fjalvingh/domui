cd7e2fc504affe0ff539f98b133f3b39b344b5d0
Frits Jalvingh <jal@etc.to>
2015-03-23 15:54:14 +0100

    MsgBox2: implement core code to ask questions using components.

--- to.etc.domui/src/to/etc/domui/component/misc/MsgBox2.java
+++ to.etc.domui/src/to/etc/domui/component/misc/MsgBox2.java
@@ -5,7 +5,6 @@ import java.util.*;
 import javax.annotation.*;
 
 import to.etc.domui.component.buttons.*;
-import to.etc.domui.component.input.*;
 import to.etc.domui.component.layout.*;
 import to.etc.domui.component.meta.*;
 import to.etc.domui.dom.css.*;
@@ -36,9 +35,37 @@ final public class MsgBox2 extends Window {
 		void onInput(T value) throws Exception;
 	}
 
+	private final class InputPair {
+		@Nullable
+		private final NodeBase	m_label;
+
+		@Nullable
+		private final NodeBase m_input;
+
+		public InputPair(@Nullable NodeBase label, @Nullable NodeBase input) {
+			if(null == label && null == input)
+				throw new IllegalStateException("Both nodes cannot be null");
+			m_label = label;
+			m_input = input;
+		}
+
+		@Nullable
+		public NodeBase getLabel() {
+			return m_label;
+		}
+
+		@Nullable
+		public NodeBase getInput() {
+			return m_input;
+		}
+	}
+
 	public static enum Type {
 		INFO, WARNING, ERROR, DIALOG, INPUT
 	}
+
+	/** Autoclose behavior. */
+	private Boolean m_autoClose;
 
 	private Img m_theImage = new Img();
 
@@ -64,8 +91,6 @@ final public class MsgBox2 extends Window {
 
 	private IInput< ? > m_oninput;
 
-	private Text< ? > m_inputControl;
-
 	/**
 	 * Custom dialog message text renderer.
 	 */
@@ -76,6 +101,9 @@ final public class MsgBox2 extends Window {
 	private MsgBoxButton	m_assumedOkButton;
 
 	private MsgBoxButton 	m_assumedCancelButton = MsgBoxButton.CANCEL;
+
+	@Nonnull
+	private List<InputPair> m_inputList = new ArrayList<>();
 
 	private MsgBox2() {
 		super(true, false, WIDTH, -1, "");
@@ -109,16 +137,32 @@ final public class MsgBox2 extends Window {
 	}
 
 	@Override public void createContent() throws Exception {
+		/*
+		 * Autoclose: default to autoclose for message/question; do not for inputs.
+		 */
+		Boolean autoClose = m_autoClose;
+		if(null == autoClose) {
+			autoClose = Boolean.valueOf(m_inputList.size() == 0);
+		}
+		setAutoClose(autoClose.booleanValue());
+
 		//-- If no buttons added: just add continue.
 		if(m_theButtons.size() == 0) {
 			button(MsgBoxButton.CONTINUE);
+
+			//-- If we have inputs then also add CANCEL
+			if(m_inputList.size() > 0) {
+				button(MsgBoxButton.CANCEL);
+			}
 		}
 
 		if(m_closeButtonObject == null) {
 			m_closeButtonObject = m_assumedCancelButton;
 		}
 		if(! m_typeSet) {
-			if(m_theButtons.size() < 2)
+			if(m_inputList.size() > 0) {
+				type(Type.DIALOG);
+			} else if(m_theButtons.size() < 2)
 				type(Type.INFO);
 			else
 				type(Type.DIALOG);
@@ -155,13 +199,11 @@ final public class MsgBox2 extends Window {
 		} else {
 			DomUtil.renderHtmlString(td, m_theText);
 		}
-		if(m_inputControl != null) {
-			td = b.addRowAndCell();
-			td.setCssClass("ui-mbx-input-1");
-			td = b.addCell();
-			td.setCssClass("ui-mbx-input");
-			td.add(m_inputControl);
-			m_inputControl.setFocus();
+
+		//-- Start adding inputs, if applicable
+		boolean unfocused = true;
+		if(m_inputList.size() > 0) {
+			unfocused = renderInputs(td);
 		}
 
 		Div bd = m_buttonDiv = new Div();
@@ -171,8 +213,52 @@ final public class MsgBox2 extends Window {
 			bd.add(btn);
 		}
 
-		if(m_inputControl == null)
+		if(unfocused)
 			setFocusOnButton();
+	}
+
+	private boolean renderInputs(@Nonnull NodeContainer nc) {
+		boolean unfocused = true;
+		Div area = new Div();
+		nc.add(area);
+		area.setCssClass("ui-mbx2-input-area");
+		TBody tb = area.addTable();
+		for(InputPair ip: m_inputList) {
+			NodeBase input = ip.getInput();
+			NodeBase label = ip.getLabel();
+
+			if(input == null) {
+				if(label != null) {
+					//-- Only label node: show label only
+					TD td = tb.addRowAndCell();
+					td.setColspan(2);
+					td.add(label);
+					td.setCssClass("ui-mbx2-in-lbl");
+				}
+			} else {
+				if(label == null) {
+					//-- No label: create an input that takes all of the area
+					TD td = tb.addRowAndCell();
+					td.setColspan(2);
+					td.add(input);
+					td.setCssClass("ui-mbx2-in-ctl");
+				} else {
+					//-- Both present
+					TD td = tb.addRowAndCell();
+					td.add(label);
+					td.setCssClass("ui-mbx2-in-lbl");
+					td = tb.addCell();
+					td.add(input);
+					td.setCssClass("ui-mbx2-in-ctl");
+				}
+
+				if(unfocused) {
+					input.setFocus();
+					unfocused = false;
+				}
+			}
+		}
+		return unfocused;
 	}
 
 	private void setFocusOnButton() {
@@ -386,47 +472,110 @@ final public class MsgBox2 extends Window {
 		return this;
 	}
 
+	public MsgBox2 autoClose(boolean auto) {
+		m_autoClose = Boolean.valueOf(auto);
+		return this;
+	}
+
 	private void setCloseButton(MsgBoxButton val) {
 		m_closeButtonObject = val;
 	}
 
 	private void answer(Object sel) throws Exception {
 		m_selectedChoice = sel;
-		if(m_onAnswer != null) {
-			try {
-				m_onAnswer.onAnswer((MsgBoxButton) m_selectedChoice);
-			} catch(ValidationException ex) {
-				//close message box in case of validation exception is thrown as result of answer
-				close();
-				throw ex;
-			}
-		}
-		if(m_onAnswer2 != null) {
-			try {
-				m_onAnswer2.onAnswer(m_selectedChoice);
-			} catch(ValidationException ex) {
-				//close message box in case of validation exception is thrown as result of answer
-				close();
-				throw ex;
-			}
-		}
-		IClicked<MsgBox2> clicked = m_clicked;
-		if(clicked != null) {
-			if(sel == m_assumedOkButton) {
-				clicked.clicked(this);
-			}
-		}
 
-		if(m_oninput != null && sel == MsgBoxButton.CONTINUE) {
-			try {
-				Object v = m_inputControl.getValue();
-				((IInput<Object>) m_oninput).onInput(v);
-			} catch(ValidationException ex) {
-				//close message box in case of validation exception is thrown as result of answer
-				close();
-				throw ex;
+		try {
+			if(m_onAnswer != null) {
+				m_onAnswer.onAnswer((MsgBoxButton) m_selectedChoice);
 			}
+			if(m_onAnswer2 != null) {
+				m_onAnswer2.onAnswer(m_selectedChoice);
+			}
+			IClicked<MsgBox2> clicked = m_clicked;
+			if(clicked != null) {
+				if(sel == m_assumedOkButton) {
+					clicked.clicked(this);
+				}
+			}
+
+			IInput<?> oninput = m_oninput;
+			if(oninput != null && sel == MsgBoxButton.CONTINUE) {
+				if(m_inputList.size() != 1)
+					throw new IllegalStateException("Internal: bad input count??");
+				InputPair inputPair = m_inputList.get(0);
+				IControl<Object> ic = (IControl<Object>) inputPair.getInput();
+				if(null == ic)
+					throw new IllegalStateException("No IControl<T> as part of IInput<> handling");
+				Object value = ic.getValue();
+				((IInput<Object>) oninput).onInput(value);
+			}
+			close();
+		} catch(ValidationException vx) {
+			throw vx;
+		} catch(Exception x) {
+			close();
+			throw x;
 		}
-		close();
+	}
+
+
+	/*--------------------------------------------------------------*/
+	/*	CODING:	Pattern: adding inputs.								*/
+	/*--------------------------------------------------------------*/
+
+	/**
+	 * Add an input line with label and control.
+	 * @param label
+	 * @param control
+	 * @return
+	 */
+	@Nonnull
+	public MsgBox2 input(@Nonnull String label, @Nonnull NodeBase control) {
+		Label l = new Label(control, label);
+		input(l, control);
+		return this;
+	}
+
+	/**
+	 * Add an input line with label and control.
+	 * @param label
+	 * @param control
+	 * @return
+	 */
+	@Nonnull
+	public MsgBox2 input(@Nonnull Label label, @Nonnull NodeBase control) {
+		_input(label, control);
+		return this;
+	}
+
+	private void _input(@Nullable NodeBase label, @Nullable NodeBase control) {
+		if(m_oninput != null)
+			throw new IllegalStateException("You cannot combine an IInput-based answer with a list-of-controls");
+
+		InputPair p = new InputPair(label, control);
+		m_inputList.add(p);
+	}
+
+	@Nonnull
+	public <T> MsgBox2 input(@Nonnull String label, @Nonnull IControl<T> control, @Nonnull IInput<T> onanswer) {
+		//-- Only allowed with input list empty
+		if(m_inputList.size() != 0)
+			throw new IllegalStateException("You cannot combine this with other input controls as there's only one answer.");
+		_input(new Label((NodeBase) control, label), (NodeBase) control);
+		m_oninput = onanswer;					// Ordered
+		return this;
+	}
+
+	@Nonnull
+	public <T> MsgBox2 input(@Nonnull IControl<T> control, @Nonnull IInput<T> onanswer) {
+		if(m_oninput != null)
+			throw new IllegalStateException("Duplicate IInput<> set");
+
+		//-- Only allowed with input list empty
+		if(m_inputList.size() != 0)
+			throw new IllegalStateException("You cannot combine this with other input controls as there's only one answer.");
+		_input(null, (NodeBase) control);
+		m_oninput = onanswer;					// Ordered
+		return this;
 	}
 }
