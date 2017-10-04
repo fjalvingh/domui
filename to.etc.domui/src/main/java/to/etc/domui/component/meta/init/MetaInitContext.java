c3f0fc566d353db4046dcc15bbf6ecbe88de4298
Frits Jalvingh <jal@etc.to>
2017-10-04 19:45:04 +0200

    [meta] Fix wrong order of per-class initialization.

--- to.etc.domui/src/main/java/to/etc/domui/component/meta/init/MetaInitContext.java
+++ to.etc.domui/src/main/java/to/etc/domui/component/meta/init/MetaInitContext.java
@@ -8,7 +8,7 @@ import to.etc.domui.component.meta.init.MetaInitializer.PropertyProviderRef;
 import javax.annotation.DefaultNonNull;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,13 +25,57 @@ final public class MetaInitContext {
 	private Map<Object, ClassInfo> m_constructionMap = new HashMap<>();
 
 	/** The list of classes that need their metadata constructed. */
-	private List<ClassInfo> m_todoProviderClassList = new ArrayList<>();
+	private List<ClassAction> m_todoProviderClassList = new ArrayList<>();
 
 	/** Indicator that something was done during a loop. */
 	private boolean m_worked;
 
+	private boolean m_markSortRequired;
+
 	public MetaInitContext(Map<Object, ClassMetaModel> classMap) {
 		m_classMap = classMap;
+	}
+
+	private final static class ClassAction {
+		private final ClassInfo m_classInfo;
+
+		private final int m_order;
+
+		@Nullable
+		private final IPropertyMetaProvider<?, ?> m_propertyProvider;
+
+		@Nullable
+		private final IClassMetaProvider<?> m_classProvider;
+
+		public ClassAction(ClassInfo ci, int order, IPropertyMetaProvider<?, ?> propertyProvider) {
+			m_classInfo = ci;
+			m_order = order;
+			m_propertyProvider = propertyProvider;
+			m_classProvider = null;
+		}
+
+		public ClassAction(ClassInfo ci, int order, IClassMetaProvider<?> classProvider) {
+			m_classInfo = ci;
+			m_order = order;
+			m_classProvider = classProvider;
+			m_propertyProvider = null;
+		}
+
+		public int getOrder() {
+			return m_order;
+		}
+
+		public ClassInfo getClassInfo() {
+			return m_classInfo;
+		}
+
+		@Nullable public IPropertyMetaProvider<?, ?> getPropertyProvider() {
+			return m_propertyProvider;
+		}
+
+		@Nullable public IClassMetaProvider<?> getClassProvider() {
+			return m_classProvider;
+		}
 	}
 
 	private static class ClassInfo {
@@ -39,9 +83,7 @@ final public class MetaInitContext {
 
 		private ClassMetaModel m_model;
 
-		private List<PropertyProviderRef> m_propertyProviderList = Collections.emptyList();
-
-		private List<ClassProviderRef> m_classProviderList = Collections.emptyList();
+		private int m_pendingActionCount;
 
 		/** The list of properties still to do for the current property provider */
 		@Nullable
@@ -54,20 +96,10 @@ final public class MetaInitContext {
 		public ClassInfo(Object type, ClassMetaModel cmm) {
 			m_type = type;
 			m_model = cmm;
-			m_propertyProviderList = new ArrayList<>(MetaInitializer.getPropertyProviderList());
-			m_classProviderList = new ArrayList<>(MetaInitializer.getClassProviderList());
 		}
 
 		public Object getType() {
 			return m_type;
-		}
-
-		public List<PropertyProviderRef> getPropertyProviderList() {
-			return m_propertyProviderList;
-		}
-
-		public List<ClassProviderRef> getClassProviderList() {
-			return m_classProviderList;
 		}
 
 		public ClassMetaModel getModel() {
@@ -88,6 +120,14 @@ final public class MetaInitContext {
 
 		public void setPropertyProvider(@Nullable IPropertyMetaProvider<?, ?> propertyProvider) {
 			m_propertyProvider = propertyProvider;
+		}
+
+		public void setPendingActionCount(int pendingActionCount) {
+			m_pendingActionCount = pendingActionCount;
+		}
+
+		boolean decrementActionCount() {
+			return --m_pendingActionCount <= 0;
 		}
 	}
 
@@ -115,10 +155,20 @@ final public class MetaInitContext {
 		IClassMetaModelFactory best = MetaInitializer.findModelFactory(type);
 		cmm = best.createModel(this, type);
 		ci = new ClassInfo(type, cmm);
-		m_todoProviderClassList.add(0, ci);
+
+		//-- Add all class actions
+		ClassInfo ci2 = ci;						// The java "architects" are morons with their final idiocy.
+		List<ClassProviderRef> classProviderList = MetaInitializer.getClassProviderList();
+		classProviderList.forEach(cp -> m_todoProviderClassList.add(new ClassAction(ci2, cp.getOrder(), cp.getProvider())));
+
+		//-- And the same for all property actions
+		List<PropertyProviderRef> propertyProviderList = MetaInitializer.getPropertyProviderList();
+		propertyProviderList.forEach(pp -> m_todoProviderClassList.add(new ClassAction(ci2, pp.getOrder(), pp.getProviderSupplier().get())));
+
+		ci.setPendingActionCount(propertyProviderList.size() + classProviderList.size());
 		m_constructionMap.put(type, ci);
+		m_markSortRequired = true;
 		return cmm;
-		//throw new ClassModelNotInitializedException(type);
 	}
 
 	/**
@@ -128,8 +178,9 @@ final public class MetaInitContext {
 		m_worked = false;
 		int notWorkCount = 0;
 		while(m_todoProviderClassList.size() > 0) {
-			handlePendingClasses();
-			if(! m_worked) {
+			int currentCount = m_todoProviderClassList.size();
+			handlePendingAction();
+			if(! m_worked && m_todoProviderClassList.size() == currentCount) {
 				notWorkCount++;
 				if(notWorkCount > 1000)
 					throw new IllegalStateException("Metadata initialization is stuck during initialization: no work could be done");
@@ -139,82 +190,74 @@ final public class MetaInitContext {
 		}
 	}
 
-	private void handlePendingClasses() throws Exception {
-		while(m_todoProviderClassList.size() > 0) {
-			ClassInfo ci = m_todoProviderClassList.get(0);
+	private void handlePendingAction() throws Exception {
+		if(m_markSortRequired) {
+			m_markSortRequired = false;
+			m_todoProviderClassList.sort(Comparator.comparingInt(ClassAction::getOrder));	// Ascending order
+		}
+		ClassAction action = m_todoProviderClassList.get(0);
+		ClassInfo ci = action.getClassInfo();
+		IClassMetaProvider<?> cp = action.getClassProvider();
+		IPropertyMetaProvider<?, ?> pp = action.getPropertyProvider();
+		boolean actionCompleted = false;
+		if(null != cp) {
+			actionCompleted = handleClassProvider((IClassMetaProvider<ClassMetaModel>) cp, ci);
+		} else if(null != pp) {
+			actionCompleted = handlePropertyProvider((IPropertyMetaProvider<ClassMetaModel, PropertyMetaModel<?>>) pp, ci);
+		} else
+			throw new IllegalStateException("Both class and property providers are empty");
 
-			for(;;) {
-				ClassProviderRef cpr = null;
-				PropertyProviderRef ppr = null;
-				if(ci.getClassProviderList().size() > 0) {
-					cpr = ci.getClassProviderList().get(0);
-				}
-				if(ci.getPropertyProviderList().size() > 0) {
-					ppr = ci.getPropertyProviderList().get(0);
-				}
-				if(ppr != null && (cpr == null || ppr.getOrder() <= cpr.getOrder())) {
-					//-- We need to execute the property provider.
-					handlePropertyProvider(ppr, ci);
-				} else if(cpr != null) {
-					handleClassProvider(cpr, ci);
-				} else if(ppr == null && cpr == null) {
-					//-- All actions for the class are done -> move it to finished
-					m_todoProviderClassList.remove(ci);			// Nothing to be done anymore
-					m_constructionMap.remove(ci.getType());		// No longer under construction.
-					m_classMap.put(ci.getType(), ci.getModel());// Store in final classmap
-					return;
-				} else {
-					throw new IllegalStateException("Logic error: no provider selected");
-				}
+		if(actionCompleted) {
+			m_todoProviderClassList.remove(action);
+			if(ci.decrementActionCount()) {
+				//-- All actions completed -> register class.
+				m_todoProviderClassList.remove(ci);			// Nothing to be done anymore
+				m_constructionMap.remove(ci.getType());		// No longer under construction.
+				m_classMap.put(ci.getType(), ci.getModel());// Store in final classmap
 			}
 		}
 	}
 
-	private void handleClassProvider(ClassProviderRef cpr, ClassInfo ci) throws Exception {
+	private boolean handleClassProvider(IClassMetaProvider<ClassMetaModel> provider, ClassInfo ci) throws Exception {
 		try {
-			IClassMetaProvider<ClassMetaModel> provider = (IClassMetaProvider<ClassMetaModel>) cpr.getProvider();
 			if(provider.getModelClass().isAssignableFrom(ci.getModel().getClass())) {
 				provider.provide(this, ci.getModel());
 			}
 			m_worked = true;
-			ci.getClassProviderList().remove(0);
+			return true;
 		} catch(ClassModelNotInitializedException cmx) {
-			queueClassLast(ci);
+			return false;
 		}
 	}
 
 	/**
 	 * Handle a property provider and call it for all properties of the class.
-	 *
-	 * @param ppr
-	 * @param ci
-	 * @throws Exception
 	 */
-	private void handlePropertyProvider(PropertyProviderRef ppr, ClassInfo ci) throws Exception {
+	private boolean handlePropertyProvider(IPropertyMetaProvider<ClassMetaModel, PropertyMetaModel<?>> newProvider, ClassInfo ci) throws Exception {
 		try {
 			//-- Do we have properties to-do?
 			List<PropertyMetaModel<?>> propList = ci.getTodoPropertyList();
-			IPropertyMetaProvider<ClassMetaModel, PropertyMetaModel<?>> provider = (IPropertyMetaProvider<ClassMetaModel, PropertyMetaModel<?>>) ci.getPropertyProvider();
+			IPropertyMetaProvider<ClassMetaModel, PropertyMetaModel<?>> currentProvider = (IPropertyMetaProvider<ClassMetaModel, PropertyMetaModel<?>>) ci.getPropertyProvider();
 			ClassMetaModel cmm = ci.getModel();
-			if(null == propList || propList.size() == 0 || provider == null) {
+			if(null == propList || propList.size() == 0 || currentProvider == null) {
 				//-- We're at the start of a new list.
-				provider = (IPropertyMetaProvider<ClassMetaModel, PropertyMetaModel<?>>) ppr.getProviderSupplier().get();		// Create the provider instance
-				if(! provider.getClassModelClass().isAssignableFrom(ci.getModel().getClass())) {
+				currentProvider = newProvider;
+				if(! currentProvider.getClassModelClass().isAssignableFrom(ci.getModel().getClass())) {
 					//-- We cannot use this-> skip
 					m_worked = true;
-					ci.getPropertyProviderList().remove(0);
-					return;
+					return true;
 				}
 
 				propList = new ArrayList<>(cmm.getProperties());
 				ci.setTodoPropertyList(propList);
-				ci.setPropertyProvider(provider);				// And store it for continuations
-				provider.beforeProperties(this, ci.getModel());
-			}
+				ci.setPropertyProvider(currentProvider);				// And store it for continuations
+				currentProvider.beforeProperties(this, ci.getModel());
+			} else if(newProvider != currentProvider)
+				throw new IllegalStateException("Continuation for property provider with new provider requested!?");
 
 			while(propList.size() > 0) {
 				PropertyMetaModel<?> pmm = propList.get(0);
-				provider.provide(this, cmm, pmm);
+				currentProvider.provide(this, cmm, pmm);
 				propList.remove(0);
 				m_worked = true;
 			}
@@ -222,16 +265,11 @@ final public class MetaInitContext {
 			//-- because we finished all properties- remove the provider.
 			ci.setPropertyProvider(null);
 			ci.setTodoPropertyList(null);
-			ci.getPropertyProviderList().remove(0);
-			provider.afterPropertiesDone(this, ci.getModel());
+			currentProvider.afterPropertiesDone(this, ci.getModel());
 			m_worked = true;										// For the uncommon case of a class without properties.
+			return true;
 		} catch(ClassModelNotInitializedException cmx) {
-			queueClassLast(ci);
+			return false;
 		}
-	}
-
-	private void queueClassLast(ClassInfo ci) {
-		m_todoProviderClassList.remove(ci);
-		m_todoProviderClassList.add(ci);						// Move to end
 	}
 }
