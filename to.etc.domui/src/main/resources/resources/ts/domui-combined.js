bbb2ce54e6cf21f1245369d0de4cb75ba373ee52
Frits Jalvingh <jal@etc.to>
2017-10-18 16:44:59 +0200

    Fix LookupInput not searching on pressing enter.

--- to.etc.domui/src/main/resources/resources/ts/domui-combined.js
+++ to.etc.domui/src/main/resources/resources/ts/domui-combined.js
@@ -117,7 +117,7 @@ var WebUI;
     function prepareAjaxCall(id, action, fields) {
         if (!fields)
             fields = new Object();
-        this.getInputFields(fields);
+        WebUI.getInputFields(fields);
         fields.webuia = action;
         fields.webuic = id;
         fields["$pt"] = window.DomUIpageTag;
@@ -3719,46 +3719,6 @@ var WebUI;
     }
     WebUI.preventSelection = preventSelection;
 })(WebUI || (WebUI = {}));
-$(function () {
-    $.getScript("$js/domui-date-checker.js");
-});
-function _block() {
-    WebUI.blockUI();
-}
-function _unblock() {
-    WebUI.unblockUI();
-}
-$(document).ajaxStart(_block).ajaxStop(_unblock);
-$(window).bind('beforeunload', function () {
-    WebUI.beforeUnload();
-    return undefined;
-});
-{
-    try {
-        var v = $.browser.version.split(".");
-        $.browser.majorVersion = parseInt(v[0], 10);
-        $.browser.minorVersion = parseInt(v[1], 10);
-        if (navigator.appName == 'Netscape') {
-            var ua = navigator.userAgent;
-            if (ua.indexOf("Trident/") != -1)
-                $.browser.msie = true;
-        }
-        if (/Edge/.test(navigator.userAgent)) {
-            $.browser.ieedge = true;
-        }
-    }
-    catch (x) { }
-}
-var DomUI = WebUI;
-$(document).ready(WebUI.onDocumentReady);
-$(window).resize(WebUI.onWindowResize);
-$(document).ajaxComplete(function () {
-    WebUI.handleCalendarChanges();
-    WebUI.doCustomUpdates();
-});
-$(document).keydown(function (e) {
-    WebUI.addPagerAccessKeys(e);
-});
 (function ($) {
     $.webui = function (xml) {
         processDoc(xml);
@@ -4183,4 +4143,44 @@ $(document).keydown(function (e) {
         return attr;
     }
 })(jQuery);
+$(function () {
+    $.getScript("$js/domui-date-checker.js");
+});
+function _block() {
+    WebUI.blockUI();
+}
+function _unblock() {
+    WebUI.unblockUI();
+}
+$(document).ajaxStart(_block).ajaxStop(_unblock);
+$(window).bind('beforeunload', function () {
+    WebUI.beforeUnload();
+    return undefined;
+});
+{
+    try {
+        var v = $.browser.version.split(".");
+        $.browser.majorVersion = parseInt(v[0], 10);
+        $.browser.minorVersion = parseInt(v[1], 10);
+        if (navigator.appName == 'Netscape') {
+            var ua = navigator.userAgent;
+            if (ua.indexOf("Trident/") != -1)
+                $.browser.msie = true;
+        }
+        if (/Edge/.test(navigator.userAgent)) {
+            $.browser.ieedge = true;
+        }
+    }
+    catch (x) { }
+}
+var DomUI = WebUI;
+$(document).ready(WebUI.onDocumentReady);
+$(window).resize(WebUI.onWindowResize);
+$(document).ajaxComplete(function () {
+    WebUI.handleCalendarChanges();
+    WebUI.doCustomUpdates();
+});
+$(document).keydown(function (e) {
+    WebUI.addPagerAccessKeys(e);
+});
 //# sourceMappingURL=domui-combined.js.map
