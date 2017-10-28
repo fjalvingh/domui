5a62dcc24ebcf88e731e6fbdce9689d2e468b75b
Frits Jalvingh <jal@etc.to>
2017-10-29 00:21:38 +0200

    [sass] Make sass compiler come from a factory, and make the vaadin compiler one of the alternatives.

---
+++ to.etc.domui/src/main/java/to/etc/domui/sass/SassCompilerFactory.java
@@ -0,0 +1,35 @@
+package to.etc.domui.sass;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on 29-10-17.
+ */
+public class SassCompilerFactory {
+	static private List<ISassCompiler> m_compilerList = Collections.emptyList();
+
+	public static ISassCompiler createCompiler() {
+		List<ISassCompiler> compilerList = getCompilerList();
+
+		for(ISassCompiler compiler : compilerList) {
+			if(compiler.available()) {
+				return compiler;
+			}
+		}
+
+		throw new IllegalStateException("There is no SASS/SCSS compiler available");
+	}
+
+	public static synchronized List<ISassCompiler> getCompilerList() {
+		return m_compilerList;
+	}
+
+	static public synchronized void register(ISassCompiler compiler) {
+		List<ISassCompiler> list = new ArrayList<>(m_compilerList);
+		list.add(compiler);
+		m_compilerList = list;
+	}
+}
