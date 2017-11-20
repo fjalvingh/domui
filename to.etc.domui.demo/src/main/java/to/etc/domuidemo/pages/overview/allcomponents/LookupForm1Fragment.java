ceda69b2f5109233e6072931383c941ec697dee3
Frits Jalvingh <jal@etc.to>
2017-11-20 21:59:40 +0100

    Make LookupForm use DateInput2, and fix the label of the LookupControl.

--- to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/overview/allcomponents/LookupForm1Fragment.java
+++ to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/overview/allcomponents/LookupForm1Fragment.java
@@ -1,10 +1,13 @@
 package to.etc.domuidemo.pages.overview.allcomponents;
 
+import to.etc.domui.component.headers.GenericHeader;
+import to.etc.domui.component.headers.GenericHeader.Type;
 import to.etc.domui.component.layout.ContentPanel;
 import to.etc.domui.component.lookup.LookupForm;
 import to.etc.domui.derbydata.db.Invoice;
 import to.etc.domui.dom.html.Div;
 import to.etc.domui.dom.html.HTag;
+import to.etc.webapp.query.QCriteria;
 
 /**
  * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
@@ -20,6 +23,19 @@ public class LookupForm1Fragment extends Div {
 		LookupForm<Invoice> lf = new LookupForm<>(Invoice.class);
 		cp.add(lf);
 
+		Div result = new Div();
+		add(result);
+
+		lf.setClicked(c -> {
+			result.removeAllChildren();
+			result.add(new GenericHeader(Type.HEADER_2, "Resulting query"));
+			QCriteria<Invoice> enteredCriteria = lf.getEnteredCriteria();
+			if(enteredCriteria == null) {
+				result.add("No data entered");
+			} else {
+				result.add(enteredCriteria.toString());
+			}
+		});
 
 	}
 }
