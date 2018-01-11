201be5d960f7ae526147ec2ddec17a72062f6dab
Frits Jalvingh <jal@etc.to>
2018-01-11 11:57:43 +0100

    Add code to allow copying text to the clipboard.

--- to.etc.domui/src/main/resources/resources/ts/domui-combined.js
+++ to.etc.domui/src/main/resources/resources/ts/domui-combined.js
@@ -1224,6 +1224,32 @@ var WebUI;
         }
     }
     WebUI.alignToMiddle = alignToMiddle;
+    function copyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.style.position = 'fixed';
+        textArea.style.top = '0';
+        textArea.style.left = '0';
+        textArea.style.width = '2em';
+        textArea.style.height = '2em';
+        textArea.style.padding = '0';
+        textArea.style.border = 'none';
+        textArea.style.outline = 'none';
+        textArea.style.boxShadow = 'none';
+        textArea.style.background = 'transparent';
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copying text command was ' + msg);
+        }
+        catch (err) {
+            console.log('Oops, unable to copy');
+        }
+        document.body.removeChild(textArea);
+    }
+    WebUI.copyTextToClipboard = copyTextToClipboard;
 })(WebUI || (WebUI = {}));
 var WebUI;
 (function (WebUI) {
