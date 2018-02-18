9862dfda478d9a898d1df53f191b8fea8e13ea9e
Ben Schoen <ben.schoen@itris.nl>
2018-02-18 17:03:54 +0100

    Quality issues fixed.

--- to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/test/RowRendererFactoryTest.java
+++ to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/test/RowRendererFactoryTest.java
@@ -5,19 +5,14 @@ import to.etc.domui.component.tbl.DataTable;
 import to.etc.domui.component.tbl.IRowControlFactory;
 import to.etc.domui.component.tbl.RowRenderer;
 import to.etc.domui.component.tbl.SimpleListModel;
-import to.etc.domui.component2.lookupinput.LookupInput2;
-import to.etc.domui.derbydata.db.Album;
 import to.etc.domui.derbydata.db.Artist;
 import to.etc.domui.dom.html.IControl;
 import to.etc.domui.dom.html.UrlPage;
 import to.etc.webapp.query.QCriteria;
 
 import javax.annotation.Nonnull;
-import java.math.BigDecimal;
 
 public class RowRendererFactoryTest extends UrlPage {
-
-	private Artist m_artist;
 
 	@Override public void createContent() throws Exception {
 		SimpleListModel<Artist> slm = new SimpleListModel<>(getSharedContext().query(QCriteria.create(Artist.class).eq("name", "Black Sabbath")));
