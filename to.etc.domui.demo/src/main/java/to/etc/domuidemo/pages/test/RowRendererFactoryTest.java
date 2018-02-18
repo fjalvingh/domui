ba0d44012cc8e85f333c2314baabb7435d938715
Frits Jalvingh <jal@etc.to>
2018-02-18 19:33:14 +0100

    Minor fixes.

--- to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/test/RowRendererFactoryTest.java
+++ to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/test/RowRendererFactoryTest.java
@@ -2,15 +2,11 @@ package to.etc.domuidemo.pages.test;
 
 import to.etc.domui.component.input.Text2;
 import to.etc.domui.component.tbl.DataTable;
-import to.etc.domui.component.tbl.IRowControlFactory;
 import to.etc.domui.component.tbl.RowRenderer;
 import to.etc.domui.component.tbl.SimpleListModel;
 import to.etc.domui.derbydata.db.Artist;
-import to.etc.domui.dom.html.IControl;
 import to.etc.domui.dom.html.UrlPage;
 import to.etc.webapp.query.QCriteria;
-
-import javax.annotation.Nonnull;
 
 public class RowRendererFactoryTest extends UrlPage {
 
@@ -25,13 +21,12 @@ public class RowRendererFactoryTest extends UrlPage {
 			}
 		).editable();
 
-		rr.column(String.class, "name").label("Editable").factory(new IRowControlFactory<Artist>() {
-			@Nonnull @Override public IControl<?> createControl(@Nonnull Artist row) throws Exception {
+		rr.column(String.class, "name").label("Editable").factory(row -> {
 				Text2<String> ctrl = new Text2<>(String.class);
 				ctrl.bind().to(row, "name");
 				return ctrl;
 			}
-		});
+		);
 
 		DataTable<Artist> dt = new DataTable<>(slm, rr);
 		add(dt);
