6124824d62c5f77501a7cac839c868040ad60785
Frits Jalvingh <jal@etc.to>
2018-02-26 14:13:51 +0100

    Logging: add the NONE level so that we can fully disable logging for some cats.

--- to.etc.logger/src/main/java/to/etc/log/Level.java
+++ to.etc.logger/src/main/java/to/etc/log/Level.java
@@ -9,7 +9,7 @@ import javax.annotation.*;
  * Created on Oct 31, 2012
  */
 public enum Level {
-	TRACE(0), DEBUG(1), INFO(2), WARN(3), ERROR(4);
+	TRACE(0), DEBUG(1), INFO(2), WARN(3), ERROR(4), NONE(10);
 
 	final int	m_code;
 
