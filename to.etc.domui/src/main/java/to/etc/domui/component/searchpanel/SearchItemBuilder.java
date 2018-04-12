25487b34a60735ce558b2c3120432b02a153f320
Frits Jalvingh <jal@etc.to>
2018-04-12 22:53:33 +0200

    Add missing QField support to SearchPanel.

--- to.etc.domui/src/main/java/to/etc/domui/component/searchpanel/SearchItemBuilder.java
+++ to.etc.domui/src/main/java/to/etc/domui/component/searchpanel/SearchItemBuilder.java
@@ -1,11 +1,12 @@
 package to.etc.domui.component.searchpanel;
 
-import to.etc.domui.component.searchpanel.lookupcontrols.ILookupQueryBuilder;
 import to.etc.domui.component.meta.PropertyMetaModel;
+import to.etc.domui.component.searchpanel.lookupcontrols.ILookupQueryBuilder;
 import to.etc.domui.dom.html.IControl;
 import to.etc.domui.dom.html.Label;
 import to.etc.domui.dom.html.NodeContainer;
 import to.etc.domui.util.IExecute;
+import to.etc.webapp.query.QField;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -87,6 +88,11 @@ final public class SearchItemBuilder<T> {
 	}
 
 	public SearchItemBuilder<T> property(String property) {
+		m_property = m_form.getMetaModel().getProperty(property);
+		return this;
+	}
+
+	public <V> SearchItemBuilder<T> property(QField<T, V> property) {
 		m_property = m_form.getMetaModel().getProperty(property);
 		return this;
 	}
