c16cfa94fe2cecbc5b7894e8f557c62272424a0c
Frits Jalvingh <jal@etc.to>
2018-11-03 16:32:29 +0100

    [fontawesome] Add more to the example page.

--- to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/overview/misc/SvgIconPage.java
+++ to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/overview/misc/SvgIconPage.java
@@ -3,6 +3,7 @@ package to.etc.domuidemo.pages.overview.misc;
 import to.etc.domui.component.buttons.DefaultButton;
 import to.etc.domui.component.buttons.LinkButton;
 import to.etc.domui.component.htmleditor.DisplayHtml;
+import to.etc.domui.component.layout.ContentPanel;
 import to.etc.domui.component.misc.EmbeddedCode;
 import to.etc.domui.component.misc.Icon;
 import to.etc.domui.component.misc.MsgBox;
@@ -24,9 +25,10 @@ public class SvgIconPage extends UrlPage {
 	}
 
 	private void addFontIcons() {
-		add(new HTag(1, "Font icon sets"));
-		add(new DisplayHtml("We can use <a target='_blank' href='https://fontawesome.com/'><b>FontAwesome</b></a> and other font icon sets easily as icons. Support for FontAwesome 4.7 and FontAwesome 5 free comes out-of-the-box."));
-		Div d = add(new Div());
+		ContentPanel cp = add(new ContentPanel());
+		cp.add(new HTag(1, "Font icon sets"));
+		cp.add(new DisplayHtml("We can use <a target='_blank' href='https://fontawesome.com/'><b>FontAwesome</b></a> and other font icon sets easily as icons. Support for FontAwesome 4.7 and FontAwesome 5 free comes out-of-the-box."));
+		Div d = cp.add(new Div());
 		d
 			.append("This uses FontAwesome 5 free icons like ")
 			.append(FaIcon.faCalculator.createNode())
@@ -36,15 +38,15 @@ public class SvgIconPage extends UrlPage {
 			.append(new EmbeddedCode("add(FaIcon.faCalculator.createNode());"))
 		;
 
-		d = add(new Div());
+		d = cp.add(new Div());
 		d.append("You can alter its size using standard css classes like: is-size-1 (big) to is-size-7 (small), or is-size-normal, is-size=-large, is-size-medium and is-size-small.");
 
-		d = add(new Div());
+		d = cp.add(new Div());
 		d
 			.append("This ").append(FaIcon.faWind.css("is-size-large").createNode()).append(" is large, for instance, done like ")
 			.append(new EmbeddedCode("add(FaIcon.faWind.css(\"is-size-large\").createNode())"));
 
-		d = add(new Div());
+		d = cp.add(new Div());
 		d.append("Font icons can be colored by using the default set of is-xxxx color names, like ")
 			.append(FaIcon.faSkullCrossbones.css("is-danger").createNode())
 			.append(", done like ")
@@ -53,15 +55,16 @@ public class SvgIconPage extends UrlPage {
 	}
 
 	private void addSvgIcons() {
-		add(new HTag(1, "SVG Icons"));
+		ContentPanel cp = add(new ContentPanel());
+		cp.add(new HTag(1, "SVG Icons"));
 
-		Div d = add(new Div());
+		Div d = cp.add(new Div());
 		d.add("This is text with a ");
 		d.add(new SvgIcon("img/xmas-tree-2.svg"));
 		d.add(" embedded as an SVG icon, using ")
 			.append(new EmbeddedCode("add(new SvgIcon(\"img/xmas-tree-2.svg\"))"));
 
-		d = add(new Div());
+		d = cp.add(new Div());
 		d.append("You can size these using size classes too, for example ");
 		d.append(new SvgIcon("img/xmas-tree-2.svg").css("is-size-1"))
 			.append(" which is done using")
@@ -69,14 +72,14 @@ public class SvgIconPage extends UrlPage {
 		;
 
 		//-- Button
-		add(new HTag(3, "Embedding into a DefaultButton"));
+		cp.add(new HTag(3, "Embedding into a DefaultButton"));
 		DefaultButton button = new DefaultButton("Click me", Icon.of("img/checkmark.svg"), a -> MsgBox.info(this, "Clicked"));
-		add(button);
+		cp.add(button);
 
 		//-- LinkButton
-		add(new HTag(3, "Inside a LinkButton"));
+		cp.add(new HTag(3, "Inside a LinkButton"));
 		LinkButton lb = new LinkButton("link button", Icon.of("img/checkmark.svg"), a -> MsgBox.info(this, "Clicked"));
-		d = add(new Div());
+		d = cp.add(new Div());
 		d.add("This is a ");
 		d.add(lb);
 		d.add(" inside text");
