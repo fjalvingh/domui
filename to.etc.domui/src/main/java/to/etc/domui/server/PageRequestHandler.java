4902039e4afe1b00ceb4c3f172e49f77a321c713
Frits Jalvingh <jal@etc.to>
2018-11-04 22:38:00 +0100

    Start moving local variables into class fields.

--- to.etc.domui/src/main/java/to/etc/domui/server/PageRequestHandler.java
+++ to.etc.domui/src/main/java/to/etc/domui/server/PageRequestHandler.java
@@ -81,12 +81,30 @@ final public class PageRequestHandler {
 
 	private final RequestContextImpl m_ctx;
 
+	private final CidPair m_cida;
+
+	@Nullable
+	private final String m_action;
+
+	private final String m_cid;
+
+	private final Class<? extends UrlPage> m_runclass;
+
 	public PageRequestHandler(DomApplication application, ApplicationRequestHandler applicationRequestHandler, ResponseCommandWriter commandWriter, PageAccessChecker checker, RequestContextImpl ctx) {
 		m_application = application;
 		m_applicationRequestHandler = applicationRequestHandler;
 		m_accessChecker = checker;
 		m_commandWriter = commandWriter;
 		m_ctx = ctx;
+
+		//-- Get core things into fields.
+		/*
+		 * If this is a full render request the URL must contain a $CID... If not send a redirect after allocating a window.
+		 */
+		m_action = ctx.getParameter(Constants.PARAM_UIACTION);			// AJAX action request?
+		String cid = m_cid = ctx.getParameter(Constants.PARAM_CONVERSATION_ID);
+		m_cida = cid == null ? null : CidPair.decodeLax(cid);
+		m_runclass = PageUtil.decodeRunClass(m_ctx);
 	}
 
 	public void executeRequest() throws Exception {
@@ -99,9 +117,8 @@ final public class PageRequestHandler {
 	}
 
 	private void handleMain() throws Exception {
-		Class< ? extends UrlPage> runclass = PageUtil.decodeRunClass(m_ctx);
 		try {
-			runClass(runclass);
+			runClass();
 		} catch(ThingyNotFoundException | ClientDisconnectedException xxxx) {
 			throw xxxx;
 		} catch(Exception x) {
@@ -130,23 +147,16 @@ final public class PageRequestHandler {
 	 * Intermediary impl; should later use interface impl on class to determine factory
 	 * to use.
 	 */
-	private void runClass(@NonNull final Class< ? extends UrlPage> clz) throws Exception {
-		/*
-		 * If this is a full render request the URL must contain a $CID... If not send a redirect after allocating a window.
-		 */
-		String action = m_ctx.getParameter(Constants.PARAM_UIACTION); 			// AJAX action request?
-		String cid = m_ctx.getParameter(Constants.PARAM_CONVERSATION_ID);
-		CidPair cida = cid == null ? null : CidPair.decodeLax(cid);
-
+	private void runClass() throws Exception {
 		if(DomUtil.USERLOG.isDebugEnabled()) {
-			DomUtil.USERLOG.debug("\n\n\n========= DomUI request =================\nCID=" + cid + "\nAction=" + action + "\n");
+			DomUtil.USERLOG.debug("\n\n\n========= DomUI request =================\nCID=" + m_cid + "\nAction=" + m_action + "\n");
 		}
-		if(!Constants.ACMD_ASYPOLL.equals(action))
+		if(!Constants.ACMD_ASYPOLL.equals(m_action))
 //			System.out.println("req: " + cid + " action " + action + ", " + m_ctx.getParameter(Constants.PARAM_UICOMPONENT));
-			logUser(cid, clz.getName(), "Incoming request on " + cid + " action=" + action);
+			logUser("Incoming request on " + m_cid + " action=" + m_action);
 
 		//-- If this is an OBITUARY just mark the window as possibly gone, then exit;
-		if(Constants.ACMD_OBITUARY.equals(action)) {
+		if(Constants.ACMD_OBITUARY.equals(m_action)) {
 			/*
 			 * Warning: do NOT access the WindowSession by findWindowSession: that updates the window touched
 			 * timestamp and causes obituary timeout handling to fail.
@@ -157,12 +167,12 @@ final public class PageRequestHandler {
 			} catch(Exception x) {
 				throw new IllegalStateException("Missing or invalid $pt PageTAG in OBITUARY request");
 			}
-			if(cida == null)
+			if(m_cida == null)
 				throw new IllegalStateException("Missing $cid in OBITUARY request");
 
 			if(LOG.isDebugEnabled())
-				LOG.debug("OBITUARY received for " + cid + ": pageTag=" + pageTag);
-			m_ctx.getSession().internalObituaryReceived(cida.getWindowId(), pageTag);
+				LOG.debug("OBITUARY received for " + m_cid + ": pageTag=" + pageTag);
+			m_ctx.getSession().internalObituaryReceived(m_cida.getWindowId(), pageTag);
 
 			//-- Send a silly response.
 			m_ctx.getOutputWriter("text/html", "utf-8");
@@ -174,25 +184,25 @@ final public class PageRequestHandler {
 
 		// ORDERED!!! Must be kept BELOW the OBITUARY check
 		WindowSession windowSession = null;
-		if(cida != null) {
-			windowSession = m_ctx.getSession().findWindowSession(cida.getWindowId());
+		if(m_cida != null) {
+			windowSession = m_ctx.getSession().findWindowSession(m_cida.getWindowId());
 		}
 
 		if(windowSession == null) {
 			boolean nonReloadableExpiredDetected = false;
-			if(action != null) {
-				if(INotReloadablePage.class.isAssignableFrom(clz)) {
+			if(m_action != null) {
+				if(INotReloadablePage.class.isAssignableFrom(m_runclass)) {
 					nonReloadableExpiredDetected = true;
 				} else {
 					// In auto refresh: do not send the "expired" message, but let the refresh handle this.
 					if(m_application.getAutoRefreshPollInterval() <= 0) {
 						String msg = Msgs.BUNDLE.getString(Msgs.S_EXPIRED);
 						m_commandWriter.generateExpired(m_ctx, msg);
-						logUser(cid, clz.getName(), msg);
+						logUser(msg);
 					} else {
-						String msg = "Not sending expired message because autorefresh is ON for " + cid;
+						String msg = "Not sending expired message because autorefresh is ON for " + m_cid;
 						LOG.info(msg);
-						logUser(cid, clz.getName(), msg);
+						logUser(msg);
 					}
 					return;
 				}
@@ -200,13 +210,13 @@ final public class PageRequestHandler {
 
 			//-- We explicitly need to create a new Window and need to send a redirect back
 			windowSession = m_ctx.getSession().createWindowSession();
-			String newmsg = "$cid: input windowid=" + cid + " not found - created wid=" + windowSession.getWindowID();
+			String newmsg = "$cid: input windowid=" + m_cid + " not found - created wid=" + windowSession.getWindowID();
 			if(LOG.isDebugEnabled())
 				LOG.debug(newmsg);
-			logUser(cid, clz.getName(), newmsg);
+			logUser(newmsg);
 
 			String conversationId = "x";							// If not reloading a saved set- use x as the default conversation id
-			if(m_application.inDevelopmentMode() && cida != null) {
+			if(m_application.inDevelopmentMode() && m_cida != null) {
 				/*
 				 * 20130227 jal The WindowSession we did not find could have been destroyed due to a
 				 * reloader event. In that case it's page shelve will be stored in the HttpSession or
@@ -219,7 +229,7 @@ final public class PageRequestHandler {
 					if(null != hs) {
 						m_ctx.internalSetWindowSession(windowSession);			// Should prevent issues when reloading
 
-						String newid = windowSession.internalAttemptReload(hs, clz, PageParameters.createFrom(m_ctx), cida.getWindowId());
+						String newid = windowSession.internalAttemptReload(hs, m_runclass, PageParameters.createFrom(m_ctx), m_cida.getWindowId());
 						if(newid != null)
 							conversationId = newid;
 					}
@@ -254,27 +264,27 @@ final public class PageRequestHandler {
 			sb.append(".").append(conversationId);
 			DomUtil.addUrlParameters(sb, m_ctx, false);
 			ApplicationRequestHandler.generateHttpRedirect(m_ctx, sb.toString(), "Your session has expired. Starting a new session.");
-			String expmsg = "Session " + cid + " has expired - starting a new session by redirecting to " + sb.toString();
-			logUser(cid, clz.getName(), expmsg);
+			String expmsg = "Session " + m_cid + " has expired - starting a new session by redirecting to " + sb.toString();
+			logUser(expmsg);
 			if(DomUtil.USERLOG.isDebugEnabled())
 				DomUtil.USERLOG.debug(expmsg);
 			return;
 		}
-		if(cida == null)
+		if(m_cida == null)
 			throw new IllegalStateException("Cannot happen: cida is null??");
-		String conversationId = cida.getConversationId();
+		String conversationId = m_cida.getConversationId();
 
 		/*
 		 * Attempt to fix etc.to bugzilla bug# 3183: IE7 sends events out of order. If an action arrives for an earlier-destroyed
 		 * conversation just ignore it, and send an empty response to ie, hopefully causing it to die soon.
 		 */
-		if(action != null) {
+		if(m_action != null) {
 			if(windowSession.isConversationDestroyed(conversationId)) {		// This conversation was recently destroyed?
 				//-- Render a null response
-				String msg = "Session " + cid + " was destroyed earlier- assuming this is an out-of-order event and sending empty delta back";
+				String msg = "Session " + m_cid + " was destroyed earlier- assuming this is an out-of-order event and sending empty delta back";
 				if(LOG.isDebugEnabled())
 					LOG.debug(msg);
-				logUser(cid, clz.getName(), msg);
+				logUser(msg);
 				System.out.println(msg);
 				m_commandWriter.generateEmptyDelta(m_ctx);
 				return;											// jal 20121122 Must return after sending that delta or the document is invalid!!
@@ -290,7 +300,7 @@ final public class PageRequestHandler {
 		 * the page has been requested with different parameters this time.
 		 */
 		PageParameters papa = null;								// Null means: ajax request, not a full page.
-		if(action == null) {
+		if(m_action == null) {
 			papa = PageParameters.createFrom(m_ctx);
 
 			//-- If this request is a huge post request - get the huge post parameters.
@@ -302,25 +312,25 @@ final public class PageRequestHandler {
 
 				papa = (PageParameters) coco.getAttribute("__ORIPP");
 				if(null == papa)
-					throw new IllegalStateException("The conversation " + cid + " no (longer) has the post data??");
+					throw new IllegalStateException("The conversation " + m_cid + " no (longer) has the post data??");
 			}
 		}
 
-		Page page = windowSession.tryToMakeOrGetPage(m_ctx, conversationId, clz, papa, action);
+		Page page = windowSession.tryToMakeOrGetPage(m_ctx, conversationId, m_runclass, papa, m_action);
 		if(page != null) {
 			page.getConversation().mergePersistentParameters(m_ctx);
 			page.internalSetPhase(PagePhase.BUILD);				// Tree can change at will
 			page.internalIncrementRequestCounter();
 			windowSession.internalSetLastPage(page);
 			if(DomUtil.USERLOG.isDebugEnabled()) {
-				DomUtil.USERLOG.debug("Request for page " + page + " in conversation " + cid);
+				DomUtil.USERLOG.debug("Request for page " + page + " in conversation " + m_cid);
 			}
 		}
 
 		/*
 		 * If this is an AJAX request make sure the page is still the same instance (session lost trouble)
 		 */
-		if(action != null) {
+		if(m_action != null) {
 			String s = m_ctx.getParameter(Constants.PARAM_PAGE_TAG);
 			if(s != null) {
 				int pt = Integer.parseInt(s);
@@ -328,21 +338,21 @@ final public class PageRequestHandler {
 					/*
 					 * The page tag differs-> session has expired.
 					 */
-					if(Constants.ACMD_ASYPOLL.equals(action)) {
+					if(Constants.ACMD_ASYPOLL.equals(m_action)) {
 						m_commandWriter.generateExpiredPollasy(m_ctx);
 					} else {
-						String msg = "Session " + cid + " expired, page will be reloaded (page tag difference) on action=" + action;
+						String msg = "Session " + m_cid + " expired, page will be reloaded (page tag difference) on action=" + m_action;
 						if(DomUtil.USERLOG.isDebugEnabled())
 							DomUtil.USERLOG.debug(msg);
-						logUser(cid, clz.getName(), msg);
+						logUser(msg);
 
 						// In auto refresh: do not send the "expired" message, but let the refresh handle this.
 						if(m_application.getAutoRefreshPollInterval() <= 0) {
 							m_commandWriter.generateExpired(m_ctx, Msgs.BUNDLE.getString(Msgs.S_EXPIRED));
 						} else {
-							msg = "Not sending expired message because autorefresh is ON for " + cid;
+							msg = "Not sending expired message because autorefresh is ON for " + m_cid;
 							LOG.info(msg);
-							logUser(cid, clz.getName(), msg);
+							logUser(msg);
 						}
 					}
 					return;
@@ -359,18 +369,18 @@ final public class PageRequestHandler {
 		/*
 		 * Handle all out-of-bound actions: those that do not manipulate UI state.
 		 */
-		if(action != null && action.startsWith("#")) {
-			runComponentAction(page, action.substring(1));
+		if(m_action != null && m_action.startsWith("#")) {
+			runComponentAction(page, m_action.substring(1));
 			return;
 			//-- If this is a PAGEDATA request - handle that
-		} else if(Constants.ACMD_PAGEDATA.equals(action)) {
+		} else if(Constants.ACMD_PAGEDATA.equals(m_action)) {
 			runPageData(page);
 			return;
 		}
 
 		//-- All commands EXCEPT ASYPOLL have all fields, so bind them to the current component data,
 		List<NodeBase> pendingChangeList = Collections.emptyList();
-		if(!Constants.ACMD_ASYPOLL.equals(action) && action != null) {
+		if(!Constants.ACMD_ASYPOLL.equals(m_action) && m_action != null) {
 			long ts = System.nanoTime();
 			pendingChangeList = handleComponentInput(page); // Move all request parameters to their input field(s)
 			if(LOG.isDebugEnabled()) {
@@ -379,8 +389,8 @@ final public class PageRequestHandler {
 			}
 		}
 
-		if(action != null) {
-			runAction(page, action, pendingChangeList);
+		if(m_action != null) {
+			runAction(page, m_action, pendingChangeList);
 			return;
 		}
 
@@ -390,17 +400,17 @@ final public class PageRequestHandler {
 		long ts = System.nanoTime();
 		try {
 			if(DomUtil.USERLOG.isDebugEnabled())
-				DomUtil.USERLOG.debug(cid + ": Full render of page " + page);
+				DomUtil.USERLOG.debug(m_cid + ": Full render of page " + page);
 
 			if(page.getBody() instanceof IRebuildOnRefresh) {                // Must fully refresh?
 				page.getBody().forceRebuild();                                // Cleanout state
 				page.setInjected(false);
 				QContextManager.closeSharedContexts(page.getConversation());
 				if(DomUtil.USERLOG.isDebugEnabled())
-					DomUtil.USERLOG.debug(cid + ": IForceRefresh, cleared page data for " + page);
-				logUser(cid, clz.getName(), "Full page render with forced refresh");
+					DomUtil.USERLOG.debug(m_cid + ": IForceRefresh, cleared page data for " + page);
+				logUser("Full page render with forced refresh");
 			} else {
-				logUser(cid, clz.getName(), "Full page render");
+				logUser("Full page render");
 			}
 			if(!page.isInjected()) {
 				m_ctx.getApplication().getInjector().injectPageValues(page.getBody(), nullChecked(papa));
@@ -438,7 +448,7 @@ final public class PageRequestHandler {
 					page.getBody().build();
 					for(UIMessage m : ml) {
 						if(DomUtil.USERLOG.isDebugEnabled())
-							DomUtil.USERLOG.debug(cid + ": page reload message = " + m.getMessage());
+							DomUtil.USERLOG.debug(m_cid + ": page reload message = " + m.getMessage());
 
 						//page.getBody().addGlobalMessage(m);
 						MessageFlare mf = MessageFlare.display(page.getBody(), m);
@@ -454,7 +464,7 @@ final public class PageRequestHandler {
 				page.getBody().build();
 				for(IGotoAction ga : al) {
 					if(DomUtil.USERLOG.isDebugEnabled())
-						DomUtil.USERLOG.debug(cid + ": page reload action = " + ga);
+						DomUtil.USERLOG.debug(m_cid + ": page reload action = " + ga);
 					ga.executeAction(page.getBody());
 				}
 				windowSession.setAttribute(UIGoto.PAGE_ACTION, null);
@@ -482,27 +492,27 @@ final public class PageRequestHandler {
 				return;
 		} catch(SessionInvalidException x) {
 			//-- Mid-air collision between logout and some other action..
-			logUser(cid, clz.getName(), "Session exception: " + x);
+			logUser("Session exception: " + x);
 			renderUserError("The session has been invalidated; perhaps you are logged out");
 			//System.err.println("domui debug: session invalidation exception");
 		} catch(ConversationDestroyedException x) {
-			logUser(cid, clz.getName(), "Conversation exception: " + x);
+			logUser("Conversation exception: " + x);
 			renderUserError("Your conversation with the server has been destroyed. Please refresh the page.");
 		} catch(Exception ex) {
 			Exception x = WrappedException.unwrap(ex);
 
 			if(!(x instanceof ValidationException)) {
-				logUser(cid, clz.getName(), "Page exception: " + x);
+				logUser("Page exception: " + x);
 			}
 
 			//-- 20100504 jal Exception in page means it's content is invalid, so force a full rebuild
 			try {
 				page.getBody().forceRebuild();
 			} catch(ConversationDestroyedException xx) {
-				logUser(cid, clz.getName(), "Conversation exception: " + xx);
+				logUser("Conversation exception: " + xx);
 				renderUserError("Your conversation with the server has been destroyed. Please refresh the page.");
 			} catch(SessionInvalidException xx) {
-				logUser(cid, clz.getName(), "Session exception: " + x);
+				logUser("Session exception: " + x);
 				renderUserError("The session has been invalidated; perhaps you have logged out in another window?");
 			} catch(Exception xxx) {
 				System.err.println("Double exception in handling full page build exception");
@@ -561,7 +571,7 @@ final public class PageRequestHandler {
 	 * </ul>
 	 */
 	private boolean checkAccess(WindowSession windowSession, Page page) throws Exception {
-		PageAccessCheckResult result = m_accessChecker.checkAccess(m_ctx, page, a -> logUser(windowSession.getWindowID(), page.getBody().getClass().getName(), a));
+		PageAccessCheckResult result = m_accessChecker.checkAccess(m_ctx, page, a -> logUser(a));
 		switch(result) {
 			default:
 				throw new IllegalArgumentException(result + "?");
@@ -589,8 +599,8 @@ final public class PageRequestHandler {
 		}
 	}
 
-	private void logUser(@Nullable String cid, @NonNull String pageName, String string) {
-		m_ctx.getSession().log(new UserLogItem(cid, pageName, null, null, string));
+	private void logUser(String string) {
+		m_ctx.getSession().log(new UserLogItem(m_cid, m_runclass.getName(), null, null, string));
 	}
 
 	private void logUser(Page page, String string) {
