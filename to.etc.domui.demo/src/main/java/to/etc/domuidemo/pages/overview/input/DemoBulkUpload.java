c8313367db06d9e09769afcf95461a98ec4e67c5
Frits Jalvingh <jal@etc.to>
2019-03-17 12:17:11 +0100

    [forms] Remove obsolete bulk upload component.

--- to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/overview/input/DemoBulkUpload.java
+++ to.etc.domui.demo/src/main/java/to/etc/domuidemo/pages/overview/input/DemoBulkUpload.java
@@ -1,11 +1,10 @@
 package to.etc.domuidemo.pages.overview.input;
 
-import to.etc.domui.component.layout.*;
-import to.etc.domui.component.misc.*;
-import to.etc.domui.component.upload.*;
-import to.etc.domui.component.upload.BulkUpload.IUpload;
-import to.etc.domui.dom.html.*;
-import to.etc.domui.util.upload.*;
+import to.etc.domui.component.layout.Caption;
+import to.etc.domui.component.misc.VerticalSpacer;
+import to.etc.domui.component.upload.FileUploadMultiple;
+import to.etc.domui.dom.html.Div;
+import to.etc.domui.dom.html.UrlPage;
 
 public class DemoBulkUpload extends UrlPage {
 	@Override
@@ -18,25 +17,8 @@ public class DemoBulkUpload extends UrlPage {
 		add(t);
 		t.setMargin("40px");
 
-		BulkUpload bu = new BulkUpload();
+		FileUploadMultiple bu = new FileUploadMultiple();
 		t.add(bu);
-		bu.setOnUpload(new IUpload() {
-			@Override
-			public boolean fileUploaded(UploadItem item) throws Exception {
-				Div d = new Div();
-				d.add("Got file " + item.getRemoteFileName() + ", " + item.getFile().length() + " bytes");
-				DemoBulkUpload.this.add(d);
-				return true;						// Delete it.
-			}
-		});
-
-		bu.setOnUploadsComplete(new IClicked<BulkUpload>() {
-
-			@Override
-			public void clicked(BulkUpload clickednode) throws Exception {
-				MsgBox.info(DemoBulkUpload.this, "All uploads have completed.");
-			}
-		});
 	}
 
 }
