427c364e389b5ec00990c8ae35c39e6a9e2507e2
Frits Jalvingh <jal@etc.to>
2019-05-08 21:44:12 +0200

    [hints] Allow rendering hints as icons in form builder.

--- to.etc.domui/src/main/java/to/etc/domui/component2/form4/ResponsiveFormLayouter.java
+++ to.etc.domui/src/main/java/to/etc/domui/component2/form4/ResponsiveFormLayouter.java
@@ -7,6 +7,8 @@ import to.etc.domui.dom.html.Label;
 import to.etc.domui.dom.html.NodeBase;
 import to.etc.domui.dom.html.NodeContainer;
 
+import java.util.function.BiConsumer;
+
 /**
  * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
  * Created on 6-3-18.
@@ -37,7 +39,8 @@ public class ResponsiveFormLayouter implements IFormLayouter {
 		m_horizontal = horizontal;
 	}
 
-	@Override public void addControl(@NonNull NodeBase control, @Nullable NodeContainer lbl, String controlCss, String labelCss, boolean append) {
+	@Override public void addControl(@NonNull NodeBase control, @Nullable NodeContainer lbl, @Nullable String hintText, String controlCss, String labelCss, boolean append,
+		BiConsumer<NodeContainer, String> hintRenderer) {
 		if(append) {
 			Div controlContainer = m_lastControlContainer;
 			Div labelContainer = m_lastLabelContainer;
@@ -70,6 +73,8 @@ public class ResponsiveFormLayouter implements IFormLayouter {
 			//-- For horizontal forms an empty label will cause the div to have 0 size, so add something to align.
 			lc.add(new Label("\u00a0"));
 		}
+		if(hintText != null)
+			hintRenderer.accept(lc, hintText);
 
 		Div cc = m_lastControlContainer = new Div("ui-f5-ctl ui-f5-ctl" + fix);
 		pair.add(cc);
