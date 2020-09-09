fcf9599bb3234cbfda70d78f4af366e6cef44da9
Vladimir Mijic <vmijic@execom.eu>
2020-09-09 17:11:32 +0200

    Added percentage converter for BigDecimal

---
+++ to.etc.domui/src/main/java/to/etc/domui/converter/PercentageBigDecimalConverter.java
@@ -0,0 +1,87 @@
+/*
+ * DomUI Java User Interface library
+ * Copyright (c) 2010 by Frits Jalvingh, Itris B.V.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * See the "sponsors" file for a list of supporters.
+ *
+ * The latest version of DomUI and related code, support and documentation
+ * can be found at http://www.domui.org/
+ * The contact for the project is Frits Jalvingh <jal@etc.to>.
+ */
+package to.etc.domui.converter;
+
+import to.etc.domui.trouble.UIException;
+import to.etc.domui.trouble.ValidationException;
+import to.etc.domui.util.Msgs;
+import to.etc.util.StringTool;
+import to.etc.webapp.nls.NlsContext;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+/**
+ * This converts a BigDecimal percentage amount to a full representation including percentage sign.
+ */
+public class PercentageBigDecimalConverter implements IConverter<BigDecimal> {
+
+	private final int m_scale;
+
+	public PercentageBigDecimalConverter(int scale) {
+		m_scale = scale;
+	}
+
+	@Override
+	public String convertObjectToString(Locale loc, BigDecimal in) throws UIException {
+		if(in == null)
+			return null;
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols(NlsContext.getLocale()); // Get numeric format symbols for the locale
+		String pattern = "##0." + StringTool.fill(m_scale, '#');
+		DecimalFormat df = new DecimalFormat(pattern, dfs);
+		return df.format(in.multiply(BigDecimal.valueOf(100))) + "%";
+	}
+
+	/**
+	 * Does a conversion of an percentage amount to a BigDecimal. The input can contain percentage value with or without trailing percentage sign.
+	 * Input value must be between 0 and 100 including. Input value is rounded to decimal digits defined by scale.
+	 *
+	 * @see IConverter#convertStringToObject(Locale, String)
+	 */
+	@Override
+	public BigDecimal convertStringToObject(Locale loc, String in) throws UIException {
+		if(in == null)
+			return null;
+
+		in = in.trim();
+		if(in.endsWith("%"))
+			in = in.substring(0, in.length() - 1).trim();
+
+		in = in.replace(',', '.'); // If percentage is entered with comma replace with dot
+
+		try {
+			return new BigDecimal(in).divide(BigDecimal.valueOf(100)).setScale(m_scale + 2, RoundingMode.HALF_EVEN);
+		} catch(NumberFormatException ex) {
+			throw new ValidationException(Msgs.V_BAD_PERCENTAGE, in);
+		}
+	}
+
+	public int getScale() {
+		return m_scale;
+	}
+}
