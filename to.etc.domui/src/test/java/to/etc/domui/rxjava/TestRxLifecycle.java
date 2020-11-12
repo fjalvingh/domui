0f5b2766f0d0cbd02fd1a183b3732eb2939460ff
Frits Jalvingh <jal@etc.to>
2020-11-12 12:12:25 +0100

    [rxjava] include rxjava in DomUI

---
+++ to.etc.domui/src/test/java/to/etc/domui/rxjava/TestRxLifecycle.java
@@ -0,0 +1,35 @@
+package to.etc.domui.rxjava;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.reactivex.rxjava3.subjects.PublishSubject;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on 12-11-20.
+ */
+public class TestRxLifecycle {
+	@Test
+	public void testLifeCycle1() throws Exception {
+		PublishSubject<String> ps = PublishSubject.<String>create();
+
+		@NonNull Observable<String> replay = ps.replay(1000).autoConnect();
+
+		Observable<String> s10 = replay
+			.doOnSubscribe(a -> {
+				System.out.println("onSubscribe called " + a + ", " + a.getClass());
+				a.dispose();
+			});
+
+		Observable<String> s20 = s10.observeOn(Schedulers.io());
+
+		s20.subscribe(a -> {
+			System.out.println(">> result: " + a);
+		});
+
+		ps.onNext("Hello");
+		Thread.sleep(2000);
+	}
+}
