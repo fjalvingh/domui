2fc15aa5c5b6bbef48a8b3c3f4abd617ffbe4125
Frits Jalvingh <jal@etc.to>
2020-12-07 10:28:26 +0100

    [timerutil] Wrap services from ScheduledExecutorService to prevent it cancelling repeating tasks that throw an exception, &*(&)^&

--- common/to.etc.alg/src/main/java/to/etc/util/TimerUtil.java
+++ common/to.etc.alg/src/main/java/to/etc/util/TimerUtil.java
@@ -4,7 +4,9 @@ import org.eclipse.jdt.annotation.Nullable;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
@@ -33,7 +35,31 @@ final public class TimerUtil {
 		return scheduler;
 	}
 
-	static public ScheduledExecutorService getTimer() {
+	static public ScheduledExecutorService getTimerService() {
 		return getScheduler();
+	}
+
+	/**
+	 * Schedule a task to execute regularly. Wraps the same method in ScheduledExecutorService to not
+	 * stop running at the first fucking exception.
+	 */
+	static public ScheduledFuture<?> scheduleAtFixedRate(long delay, long period, TimeUnit unit, Runnable thingy) {
+		Runnable wrappedRunnable = () -> {
+			try {
+				thingy.run();
+			} catch(Throwable x) {
+				System.err.println("[timerutil] run failed: " + x);
+				x.printStackTrace();
+			}
+		};
+		return getScheduler().scheduleAtFixedRate(wrappedRunnable, delay, period, unit);
+	}
+
+	static public ScheduledFuture<?> schedule(long delay, TimeUnit unit, Runnable what) {
+		return getScheduler().schedule(what, delay, unit);
+	}
+
+	static public void shutdownNow() {
+		getScheduler().shutdownNow();
 	}
 }
