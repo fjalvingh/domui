e44f3ac851ec9e215e8f5c24a268fd0453fdf657
Stefan Katic <skatic@execom.eu>
2020-12-09 12:23:24 +0100

    Remove time from yearend.

--- common/to.etc.alg/src/main/java/to/etc/util/DateUtil.java
+++ common/to.etc.alg/src/main/java/to/etc/util/DateUtil.java
@@ -408,10 +408,7 @@ final public class DateUtil {
 
 	static public Date yearEndDate(int year) {
 		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.HOUR_OF_DAY, 23);
-		cal.set(Calendar.MINUTE, 59);
-		cal.set(Calendar.SECOND, 59);
-		cal.set(Calendar.MILLISECOND, 999);
+		clearTime(cal);
 		cal.set(Calendar.YEAR, year);
 		cal.set(Calendar.MONTH, 11);
 		cal.set(Calendar.DAY_OF_MONTH, 31);
