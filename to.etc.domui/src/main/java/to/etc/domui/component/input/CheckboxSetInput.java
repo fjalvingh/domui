68c449bc4c3c5b3887a27c06cc00b1257bc8fa0a
Frits Jalvingh <jal@etc.to>
2021-01-20 11:28:50 +0100

    Add simple method to add enumerated checkboxset

--- to.etc.domui/src/main/java/to/etc/domui/component/input/CheckboxSetInput.java
+++ to.etc.domui/src/main/java/to/etc/domui/component/input/CheckboxSetInput.java
@@ -1,7 +1,9 @@
 package to.etc.domui.component.input;
 
 import org.eclipse.jdt.annotation.NonNull;
+import to.etc.domui.util.DomUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CheckboxSetInput<T> extends CheckboxSetInputBase<T, T> {
@@ -29,4 +31,14 @@ public class CheckboxSetInput<T> extends CheckboxSetInputBase<T, T> {
 		return this;
 	}
 
+	static public <T extends Enum<T>> CheckboxSetInput<T> create(Class<T> clz, T... exceptions) {
+		T[] ar = clz.getEnumConstants();
+		List<T> l = new ArrayList<>(ar.length);
+		for(T v : ar) {
+			if(!DomUtil.contains(exceptions, v)) {
+				l.add(v);
+			}
+		}
+		return new CheckboxSetInput<T>(l);
+	}
 }
