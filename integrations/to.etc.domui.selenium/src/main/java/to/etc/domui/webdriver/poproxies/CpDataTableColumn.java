5b52b6f45d0091d8dd32af360aaea1d9b6505605
Frits Jalvingh <jal@etc.to>
2021-12-10 15:49:37 +0100

    [pogen] Fix CpDataTableColumn.

--- integrations/to.etc.domui.selenium/src/main/java/to/etc/domui/webdriver/poproxies/CpDataTableColumn.java
+++ integrations/to.etc.domui.selenium/src/main/java/to/etc/domui/webdriver/poproxies/CpDataTableColumn.java
@@ -5,4 +5,12 @@ package to.etc.domui.webdriver.poproxies;
  * Created on 09-12-21.
  */
 public class CpDataTableColumn {
+	private final CpDataTable<?> m_dataTable;
+
+	private final int m_columnIndex;
+
+	public CpDataTableColumn(CpDataTable<?> dataTable, int columnIndex) {
+		m_dataTable = dataTable;
+		m_columnIndex = columnIndex;
+	}
 }
