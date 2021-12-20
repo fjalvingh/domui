ec3130767677e1fc1a364170cd243140995697f7
Vladimir Mijic <millevlada@gmail.com>
2021-12-20 17:50:33 +0100

    Added initial support for client side certification.

--- common/to.etc.alg/src/main/java/to/etc/net/SslCertificateType.java
+++ common/to.etc.alg/src/main/java/to/etc/net/SslCertificateType.java
@@ -1,14 +1,19 @@
 package to.etc.net;
 
+import java.util.List;
+
+import static java.util.List.of;
+
 public enum SslCertificateType {
-	PFX("SunX509", "PKCS12", "pfx")
-	;
+	PFX("SunX509", "PKCS12", of(".pfx"));
 
 	private final String m_keyManagerAlgorithm;
-	private final String m_keyStoreType;
-	private final String m_extensions;
 
-	SslCertificateType(String keyManagerAlgorithm, String keyStoreType, String extensions) {
+	private final String m_keyStoreType;
+
+	private final List<String> m_extensions;
+
+	SslCertificateType(String keyManagerAlgorithm, String keyStoreType, List<String> extensions) {
 		m_keyManagerAlgorithm = keyManagerAlgorithm;
 		m_keyStoreType = keyStoreType;
 		m_extensions = extensions;
@@ -22,7 +27,7 @@ public enum SslCertificateType {
 		return m_keyStoreType;
 	}
 
-	public String getExtensions() {
+	public List<String> getExtensions() {
 		return m_extensions;
 	}
 }
