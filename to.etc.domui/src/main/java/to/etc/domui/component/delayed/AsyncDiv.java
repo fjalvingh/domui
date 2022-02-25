9acf9a91523af0d4303bab1551aa622b8e1a34ad
Frits Jalvingh <jal@etc.to>
2022-02-25 15:39:38 +0100

    [sm][ssv] Implement the single test part including UI.

--- to.etc.domui/src/main/java/to/etc/domui/component/delayed/AsyncDiv.java
+++ to.etc.domui/src/main/java/to/etc/domui/component/delayed/AsyncDiv.java
@@ -1,11 +1,15 @@
 package to.etc.domui.component.delayed;
 
+import to.etc.domui.component.layout.MessageLine;
 import to.etc.domui.component.misc.MsgBox;
+import to.etc.domui.dom.errors.MsgType;
 import to.etc.domui.dom.html.Div;
 import to.etc.domui.dom.html.HTag;
+import to.etc.domui.dom.html.Para;
 import to.etc.domui.util.Msgs;
 import to.etc.parallelrunner.IAsyncRunnable;
 import to.etc.util.CancelledException;
+import to.etc.util.MessageException;
 import to.etc.util.StringTool;
 
 /**
@@ -22,6 +26,11 @@ abstract public class AsyncDiv<T extends IAsyncRunnable> extends Div {
 		m_what = what;
 	}
 
+	public AsyncDiv(T runnable) {
+		m_runnable = runnable;
+		m_what = null;
+	}
+
 	@Override
 	final public void createContent() throws Exception {
 		String what = m_what;
@@ -31,13 +40,7 @@ abstract public class AsyncDiv<T extends IAsyncRunnable> extends Div {
 		add(new AsyncContainer(m_runnable, (cancelled, errorException) -> {
 			//-- If we've got an exception replace the contents with the exception message.
 			if(errorException != null && !(errorException instanceof CancelledException)) {
-				//errorException.printStackTrace();
-				StringBuilder sb = new StringBuilder(8192);
-				StringTool.strStacktrace(sb, errorException);
-				String s = sb.toString();
-				s = s.replace("\n", "<br/>\n");
-
-				MsgBox.error(this, Msgs.BUNDLE.getString(Msgs.ASYNC_ERROR) + "<br/>" + s);
+				createError(m_runnable, errorException, false);
 			} else if(cancelled) {
 				MsgBox.info(this, Msgs.BUNDLE.getString(Msgs.ASYNC_CONTAINER_CANCELLED));
 			} else {
@@ -45,6 +48,25 @@ abstract public class AsyncDiv<T extends IAsyncRunnable> extends Div {
 				createContent(m_runnable);
 			}
 		}));
+	}
+
+	protected void createError(T task, Exception errorException, boolean cancelled) throws Exception {
+		if(cancelled) {
+			add(new MessageLine(MsgType.INFO, Msgs.BUNDLE.getString(Msgs.ASYNC_CONTAINER_CANCELLED)));
+		} else if(errorException instanceof MessageException) {
+			add(new MessageLine(MsgType.ERROR, errorException.getMessage()));
+		} else {
+			add(new MessageLine(MsgType.ERROR, Msgs.BUNDLE.getString(Msgs.ASYNC_ERROR)));
+
+			StringBuilder sb = new StringBuilder(8192);
+			StringTool.strStacktrace(sb, errorException);
+			String s = sb.toString();
+			s = s.replace("\n", "<br/>\n");
+
+			Para p = new Para();
+			add(p);
+			p.setText(s);
+		}
 	}
 
 	abstract public void createContent(T task) throws Exception;
