dd58cba80bdfa253ac0490cba71fb42e3092bb8c
Vladimir Mijic <millevlada@gmail.com>
2022-05-09 15:54:10 +0200

    refactoring of augment processing to handle target db late

--- common/to.etc.alg/src/main/java/to/etc/parallelrunner/simple/ParallelTaskRunner.java
+++ common/to.etc.alg/src/main/java/to/etc/parallelrunner/simple/ParallelTaskRunner.java
@@ -2,17 +2,15 @@ package to.etc.parallelrunner.simple;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import to.etc.parallelrunner.AsyncWorker;
 import to.etc.parallelrunner.IAsyncRunnable;
 import to.etc.parallelrunner.simple.ParallelTaskRunner.IAsyncRunnableSimpleTask;
 import to.etc.util.Progress;
+import to.etc.util.WrappedException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Collectors;
 
 public final class ParallelTaskRunner<T extends IAsyncRunnableSimpleTask> {
 
@@ -27,13 +25,11 @@ public final class ParallelTaskRunner<T extends IAsyncRunnableSimpleTask> {
 		void onAfterExecuted(boolean cancelled, @Nullable Exception exception);
 	}
 
-	private final AsyncWorker m_asyncWorker;
-
 	private Progress m_progress;
 
-	private List<SimpleTaskExecutor<T>> m_idleExecutors;
+	private final List<SimpleTaskExecutor<T>> m_idleExecutors;
 
-	private List<SimpleTaskExecutor<T>> m_busyExecutors = new ArrayList<>();
+	private final List<SimpleTaskExecutor<T>> m_allExecutors = new ArrayList<>();
 
 	private final ReentrantLock m_lock = new ReentrantLock();
 
@@ -47,14 +43,15 @@ public final class ParallelTaskRunner<T extends IAsyncRunnableSimpleTask> {
 
 	private int m_tasksCompleted = 0;
 
-	public ParallelTaskRunner(Progress p, int capacity, AsyncWorker asyncWorker) {
+	public ParallelTaskRunner(Progress p, int capacity) {
 		m_progress = p;
-		m_asyncWorker = asyncWorker;
 
 		m_idleExecutors = new ArrayList<>();
 		for (int i = 1; i <= capacity; i++) {
 			SimpleTaskExecutor<T> executor = new SimpleTaskExecutor<>(this, i);
 			m_idleExecutors.add(executor);
+			m_allExecutors.add(executor);
+			executor.start();
 		}
 	}
 
@@ -68,10 +65,9 @@ public final class ParallelTaskRunner<T extends IAsyncRunnableSimpleTask> {
 				m_hasIdleExecutorsCondition.await();
 			}
 			SimpleTaskExecutor simpleTaskExecutor = m_idleExecutors.remove(0);
-			m_busyExecutors.add(simpleTaskExecutor);
 			m_tasksAdded++;
 			//System.out.println("Added to m_busyExecutors, size: " + m_busyExecutors.size());
-			simpleTaskExecutor.schedule(m_progress, task);
+			simpleTaskExecutor.schedule(task);
 		}finally {
 			m_lock.unlock();
 		}
@@ -80,12 +76,11 @@ public final class ParallelTaskRunner<T extends IAsyncRunnableSimpleTask> {
 	void reportTaskDone(SimpleTaskExecutor<T> executor, T task, boolean cancelled, Exception ex) {
 		m_lock.lock();
 		try {
-			m_busyExecutors.remove(executor);
 			m_tasksCompleted++;
 			//System.out.println("Removed from m_busyExecutors, size: " + m_busyExecutors.size());
 			m_idleExecutors.add(executor);
 			m_hasIdleExecutorsCondition.signal();
-			if(m_busyExecutors.isEmpty() && m_allTasksAdded) {
+			if(m_idleExecutors.size() == m_allExecutors.size() && m_allTasksAdded) {
 				//System.out.println("Signaling m_noBusyExecutorsCondition!");
 				m_noBusyExecutorsCondition.signal();
 			}
@@ -109,7 +104,7 @@ public final class ParallelTaskRunner<T extends IAsyncRunnableSimpleTask> {
 		m_lock.lock();
 		try {
 			for(;;) {
-				if(m_busyExecutors.isEmpty()) {
+				if(m_idleExecutors.size() == m_allExecutors.size()) {
 					break;
 				}
 				m_noBusyExecutorsCondition.await();
@@ -117,24 +112,34 @@ public final class ParallelTaskRunner<T extends IAsyncRunnableSimpleTask> {
 		}finally {
 			m_lock.unlock();
 		}
+		for(SimpleTaskExecutor<T> executor: m_allExecutors) {
+			executor.signalTerminate();
+		}
+		for(SimpleTaskExecutor<T> executor: m_allExecutors) {
+			executor.join();
+		}
+
 		System.out.println("** All finished");
 	}
 
-	protected void onTaskStarted(T task) {
+	public void cancelAll() throws Exception {
+		for(SimpleTaskExecutor<T> executor: m_allExecutors) {
+			executor.signalTerminate();
+		}
+		for(SimpleTaskExecutor<T> executor: m_allExecutors) {
+			executor.join();
+		}
+
+		System.out.println("** Cancel completed");
 	}
 
-	protected void onFinish(T task) {
-	}
-
-	protected void handleCancelledAfter(T task) {
-	}
-
-	boolean isCancelled() {
-		return Objects.requireNonNull(m_progress).isCancelled();
-	}
-
-	public synchronized List<T> getRunningTasks() {
-		return m_busyExecutors.stream().map(it -> it.getTask()).collect(Collectors.toList());
+	public int getNumberOfRunningExecutors() {
+		m_lock.lock();
+		try {
+			return m_allExecutors.size() - m_idleExecutors.size();
+		} finally {
+			m_lock.unlock();
+		}
 	}
 
 	public int getTasksAdded() {
@@ -148,43 +153,82 @@ public final class ParallelTaskRunner<T extends IAsyncRunnableSimpleTask> {
 	/**
 	 * The executor that runs tasks provided by runner.
 	 */
-	static final public class SimpleTaskExecutor<V extends IAsyncRunnableSimpleTask>  {
+	static final public class SimpleTaskExecutor<V extends IAsyncRunnableSimpleTask> extends Thread {
 		private final ParallelTaskRunner<V> m_runner;
 
+		@Nullable
 		private V m_task;
 
 		private final int m_index;
 
+		private boolean m_terminate;
+
 		public SimpleTaskExecutor(ParallelTaskRunner<V> runner, int index) {
+			super();
+			setDaemon(true);
+			setName("pt" + index);
 			m_runner = runner;
 			m_index = index;
 		}
 
-		public void schedule(Progress p, V task) {
-			m_task = task;
-			//System.out.println("Scheduling task: " + toString());
-			task.onBeforeScheduled();
-			m_runner.m_asyncWorker.schedule(toString(), task, (cancelled, exception) -> {
-				m_task = null;
-				task.onAfterExecuted(cancelled, exception);
-				m_runner.reportTaskDone(this, task, cancelled, exception);
-			});
-		}
-
-		@Nullable
-		public V getTask() {
-			return m_task;
+		public void schedule(V task) {
+			synchronized(this) {
+				task.onBeforeScheduled();
+				m_task = task;
+				notifyAll();
+			}
 		}
 
 		@Override public String toString() {
 			V task = m_task;
-			String me = "T" + m_index + ": ";
+			String me = getName() + ": ";
 			if(null == task) {
 				me += "idle";
 			}else {
 				me += task.toString();
 			}
 			return me;
+		}
+
+		@Override
+		public void run() {
+			for(;;) {
+				V task;
+				synchronized(this) {
+					for(;;) {
+						if(m_terminate) {
+							return;
+						}
+						task = m_task;
+						if(null != task) {
+							m_task = null;
+							break;
+						}
+						try {
+							wait();
+						} catch(InterruptedException e) {
+							e.printStackTrace();
+							throw WrappedException.wrap(e);
+						}
+					}
+				}
+
+				try {
+					task.run(m_runner.m_progress);
+					m_runner.reportTaskDone(this, task, false, null);
+					task.onAfterExecuted(false, null);
+				} catch(Exception e) {
+					m_runner.reportTaskDone(this, task, false, e);
+					task.onAfterExecuted(false, e);
+				}
+			}
+		}
+
+		void signalTerminate() {
+			synchronized(this) {
+				m_terminate = true;
+				notifyAll();
+			}
 		}
 	}
 }
