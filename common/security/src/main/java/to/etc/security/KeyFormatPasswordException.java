c19d42f198b1c5813f459c5394d0532797fa4b1d
Frits Jalvingh <jal@etc.to>
2023-01-11 16:38:38 +0100

    [sshkeys] Add more methods to decode more of the idiotic and badly described encrypted key formats

---
+++ common/security/src/main/java/to/etc/security/KeyFormatPasswordException.java
@@ -0,0 +1,11 @@
+package to.etc.security;
+
+/**
+ * @author <a href="mailto:jal@etc.to">Frits Jalvingh</a>
+ * Created on 11-01-23.
+ */
+public class KeyFormatPasswordException extends KeyFormatException {
+	public KeyFormatPasswordException(String message) {
+		super(message);
+	}
+}
