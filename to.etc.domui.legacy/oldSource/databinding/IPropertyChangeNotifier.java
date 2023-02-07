392f75b2cfa9868c832692ea2b8e91bcefd3efba
Frits Jalvingh <jal@etc.to>
2023-02-07 12:10:43 +0100

    Remove more old components

--- to.etc.domui.legacy/oldSource/databinding/IPropertyChangeNotifier.java
+++
@@ -1,7 +0,0 @@
-package to.etc.domui.databinding;
-
-import org.eclipse.jdt.annotation.*;
-
-public interface IPropertyChangeNotifier {
-	public <V> void notifyIfChanged(@Nullable V old, @Nullable V value);
-}
