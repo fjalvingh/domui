358614c4dbb564289e0b1076e2243826a1795239
Jovana Zivanovic <jovana.zivanovic@htecgroup.com>
2023-06-13 13:27:10 +0200

    added test helper methods

--- integrations/to.etc.domui.selenium/src/main/java/to/etc/domui/webdriver/poproxies/CpComboLookup2.java
+++ integrations/to.etc.domui.selenium/src/main/java/to/etc/domui/webdriver/poproxies/CpComboLookup2.java
@@ -1,6 +1,7 @@
 package to.etc.domui.webdriver.poproxies;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.openqa.selenium.support.ui.Select;
 import to.etc.domui.webdriver.core.WebDriverConnector;
 
 import java.util.function.Supplier;
@@ -9,5 +10,11 @@ import java.util.function.Supplier;
 public class CpComboLookup2 extends AbstractCpComboComponent implements ICpControl<String> {
 	public CpComboLookup2(WebDriverConnector connector, Supplier<String> selectorProvider) {
 		super(connector, selectorProvider);
+	}
+
+	public Select getOptions() {
+		var element = wd().getElement(getSelector());
+		Select option = new Select(element);
+		return option;
 	}
 }
