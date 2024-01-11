8c22c07c957e791183ee499fa08e839ca4ffeccb
Frits Jalvingh <jal@etc.to>
2024-01-11 07:26:28 +0100

    [vmm] Rename restore tool and make it multicommand; fix size reporting. Remove Progress*Stream things and merge, simplified, into SizeCounting*Stream classes. Fix tar util to report both uncompressed and compressed bytes.

--- common/to.etc.alg/src/test/java/to/etc/util/CompressedTarUtilTest.java
+++ common/to.etc.alg/src/test/java/to/etc/util/CompressedTarUtilTest.java
@@ -18,7 +18,7 @@ public class CompressedTarUtilTest {
 		try(
 			FileOutputStream fos = new FileOutputStream(tarName);
 		) {
-			CompressedTarUtil.tar(fos, it -> System.out.println("size so far: " + it), rootLocation, "NT15_BZK_dVi_20210120.b - testsuite");
+			CompressedTarUtil.tar(fos, (uncompressed, compressed) -> System.out.println("compressed size so far: " + compressed), rootLocation, "NT15_BZK_dVi_20210120.b - testsuite");
 		}
 		File tarFile = new File(tarName);
 		System.out.println("Completed, created " + tarFile.getAbsolutePath() + ", of size: " + StringTool.strSize(tarFile.length()));
@@ -29,7 +29,7 @@ public class CompressedTarUtilTest {
 		File targzFile = new File("/home/vmijic/Downloads/NT15_BZK_DVI_20210120.b Berichten/berichten1/berichten.tar.gz");
 		File extractLocation = new File("/home/vmijic/Downloads/NT15_BZK_DVI_20210120.b Berichten/berichten2/");
 		try(FileInputStream fis = new FileInputStream(targzFile)) {
-			CompressedTarUtil.untar(extractLocation, fis, it -> System.out.println("size so far: " + it));
+			CompressedTarUtil.untar(extractLocation, fis, (uncompressed, compressed) -> System.out.println("size so far: " + uncompressed + " uncompressed"));
 		}
 		System.out.println("Completed!");
 	}
