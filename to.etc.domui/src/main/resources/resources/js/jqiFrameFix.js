5bdf07421951930ccf44dd0a759ee2faa5437e69
mamo <marc.mol@bloomville.nl>
2025-02-26 11:12:35 +0100

    Sizing of iFrame with totara page was incorrect when showing on device with a width < 912.

--- to.etc.domui/src/main/resources/resources/js/jqiFrameFix.js
+++ to.etc.domui/src/main/resources/resources/js/jqiFrameFix.js
@@ -2,33 +2,34 @@
 	$.fn.addIframe = function(iframeHtml) {
 		return this.each(function() {
 
-			var tempDiv = document.createElement('div');
-			var newIframe = document.createElement('iframe');
-
 			var srcMatch = iframeHtml.match(/src=["']([^"']+)["']/);
 			var styleMatch = iframeHtml.match(/style=["']([^"']+)["']/);
 
-			if (srcMatch) {
-				newIframe.src = srcMatch[1];
-			}
-
 			if (styleMatch) {
+				var width = 600;
+				var height = 400;
 				var styles = styleMatch[1].split(';');
 				styles.forEach(style => {
 					var [property, value] = style.split(':').map(s => s.trim());
 					if (property === 'width') {
-						newIframe.width = value;
+						width = value;
 					} else if (property === 'height') {
-						newIframe.height = value;
+						height = value;
 					}
 				});
-			} else {
-				// Default width en height als ze niet zijn opgegeven
-				newIframe.width = '600';
-				newIframe.height = '400';
 			}
-			tempDiv.appendChild(newIframe);
-			$(this).before(tempDiv);
+
+			let newIframe = $("<iframe>", {
+				src: srcMatch[1],
+				width: width,
+				height: height,
+				frameborder: "0"
+			}).css({
+				width: width,
+				height: height
+			});
+
+			$(this).append(newIframe);
 		});
 	};
 })(jQuery);
