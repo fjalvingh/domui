eb93f1c06f831be69d0897118dfe56d436602190
Frits Jalvingh <jal@etc.to>
2025-07-15 17:31:52 +0200

    [stringtool] Allow setting the substitution consts for the Levenshtein distance calculcation

--- common/to.etc.alg/src/main/java/to/etc/util/StringTool.java
+++ common/to.etc.alg/src/main/java/to/etc/util/StringTool.java
@@ -603,6 +603,10 @@ public class StringTool {
 	 * those strings.
 	 */
 	public static int getLevenshteinDistance(@NonNull String s, @NonNull String t, boolean ignorecase) {
+		return getLevenshteinDistance(s, t, ignorecase, 1);
+	}
+
+	public static int getLevenshteinDistance(@NonNull String s, @NonNull String t, boolean ignorecase, int substitutionCost) {
 		if(s == null || t == null)
 			throw new IllegalArgumentException("Strings must not be null");
 		if(ignorecase) {
@@ -642,7 +646,7 @@ public class StringTool {
 			d[0] = j;
 
 			for(i = 1; i <= n; i++) {
-				cost = s.charAt(i - 1) == t_j ? 0 : 1;
+				cost = s.charAt(i - 1) == t_j ? 0 : substitutionCost;
 				//-- minimum of cell to the left+1, to the top+1, diagonally left and up +cost
 				d[i] = Math.min(Math.min(d[i - 1] + 1, p[i] + 1), p[i - 1] + cost);
 			}
