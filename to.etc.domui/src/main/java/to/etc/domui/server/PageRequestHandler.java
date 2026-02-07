5355a9ee3673da5259654a31fc336927d8cc2e25
Frits Jalvingh <jal@etc.to>
2026-02-07 16:10:25 +0100

    [sonar] cherry-pick removal of old json renderers

--- to.etc.domui/src/main/java/to/etc/domui/server/PageRequestHandler.java
+++ to.etc.domui/src/main/java/to/etc/domui/server/PageRequestHandler.java
@@ -1,5 +1,6 @@
 package to.etc.domui.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
@@ -49,12 +50,9 @@ import to.etc.domui.util.INewPageInstantiated;
 import to.etc.domui.util.IRebuildOnRefresh;
 import to.etc.domui.util.Msgs;
 import to.etc.function.ConsumerEx;
-import to.etc.util.IndentWriter;
 import to.etc.util.StringTool;
 import to.etc.util.WrappedException;
 import to.etc.webapp.ProgrammerErrorException;
-import to.etc.webapp.ajax.renderer.json.JSONRegistry;
-import to.etc.webapp.ajax.renderer.json.JSONRenderer;
 import to.etc.webapp.query.QContextManager;
 
 import javax.servlet.http.HttpSession;
@@ -937,9 +935,6 @@ final public class PageRequestHandler {
 		}
 	}
 
-	@NonNull
-	final private JSONRegistry m_jsonRegistry = new JSONRegistry();
-
 	private void renderJsonLikeResponse(Page page, @NonNull Object value) throws Exception {
 		m_ctx.renderResponseHeaders(page.getBody());
 		if(value instanceof IDataFactory) {
@@ -953,9 +948,8 @@ final public class PageRequestHandler {
 			//-- String return: we'll assume this is a javascript response by itself.
 			w.write((String) value);
 		} else {
-			//-- Object return: render as JSON
-			JSONRenderer jr = new JSONRenderer(m_jsonRegistry, new IndentWriter(w), false);
-			jr.render(value);
+			ObjectMapper om = new ObjectMapper();
+			om.writeValue(w, value);
 		}
 	}
 
