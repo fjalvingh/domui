330bc6287209cf45ce4edd958d8f83b899ed20c1
Frits Jalvingh <jal@etc.to>
2026-02-23 15:53:46 +0100

    [jakarta] Fix test db script to be able to properly insert data

--- to.etc.domui.derbydata/src/main/java/to/etc/domui/derbydata/db/Artist.java
+++ to.etc.domui.derbydata/src/main/java/to/etc/domui/derbydata/db/Artist.java
@@ -3,6 +3,8 @@ package to.etc.domui.derbydata.db;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.OneToMany;
@@ -30,6 +32,7 @@ public class Artist extends DbRecordBase<Long> {
 	@Override
 	@Id
 	@SequenceGenerator(name = "sq", sequenceName = "artist_sq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sq")
 	@Column(name = "ArtistId", nullable = false, precision = 20)
 	public Long getId() {
 		return m_id;
