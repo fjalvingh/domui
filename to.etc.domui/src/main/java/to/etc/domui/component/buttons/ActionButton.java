9aa8dc00df10045eace4ab404a377a1cd6eb7de2
Frits Jalvingh <jal@etc.to>
2026-09-06 16:04:22 +0200

    [cleanup] Fix hanging typedefs

--- to.etc.domui/src/main/java/to/etc/domui/component/buttons/ActionButton.java
+++ to.etc.domui/src/main/java/to/etc/domui/component/buttons/ActionButton.java
@@ -26,7 +26,7 @@ public class ActionButton extends DefaultButton {
 
 	private final List<IUIAction> m_actions = new ArrayList<>();
 
-	public <T> ActionButton(IUIAction action) throws Exception {
+	public ActionButton(IUIAction action) throws Exception {
 		super(action);
 	}
 
